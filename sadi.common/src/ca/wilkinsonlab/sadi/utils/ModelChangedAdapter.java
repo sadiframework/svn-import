1a3e286a9a68b24334d12122892517384cd0d99f
elmccarthy <elmccarthy@gmail.com>
2009-06-05 23:35:56 +0000

    Added logging to ModelChangedAdapter.

--- sadi.common/src/ca/wilkinsonlab/sadi/utils/ModelChangedAdapter.java
+++ sadi.common/src/ca/wilkinsonlab/sadi/utils/ModelChangedAdapter.java
@@ -20,54 +20,77 @@ public abstract class ModelChangedAdapter implements ModelChangedListener
 
 	public void addedStatements(Statement[] statements)
 	{
+		if (log.isTraceEnabled())
+			log.trace( String.format("adding %d statements from array ", statements.length) );
+		
 		for (Statement s: statements)
 			addedStatement(s);
 	}
 
 	public void addedStatements(List statements)
 	{
+		if (log.isTraceEnabled())
+			log.trace( String.format("adding %d statements from list ", statements.size()) );
+		
 		for (Object o: statements)
 			addedStatement((Statement)o);
 	}
 
 	public void addedStatements(StmtIterator statements)
 	{
+		if (log.isTraceEnabled())
+			log.trace( "adding statements from iterator" );
+		
 		while (statements.hasNext())
 			addedStatement(statements.nextStatement());
 	}
 
 	public void addedStatements(Model m)
 	{
+		if (log.isTraceEnabled())
+			log.trace( "adding statements from model" );
+		
 		addedStatements(m.listStatements());
 	}
 
 	public void notifyEvent(Model m, Object event)
 	{
-		if (false)
-			log.warn(String.format("unexpected call to notifyEvent(%s, %s)", m, event),
-					new UnsupportedOperationException());
+		if (log.isTraceEnabled())
+			log.trace( String.format("notified of event %s", event) );
 	}
 
 	public void removedStatements(Statement[] statements)
 	{
+		if (log.isTraceEnabled())
+			log.trace( String.format("removing %d statements from array ", statements.length) );
+		
 		for (Statement s: statements)
 			removedStatement(s);
 	}
 
 	public void removedStatements(List statements)
 	{
+		if (log.isTraceEnabled())
+			log.trace( String.format("removing %d statements from list ", statements.size()) );
+		
 		for (Object o: statements)
 			removedStatement((Statement)o);
 	}
 
 	public void removedStatements(StmtIterator statements)
 	{
+		if (log.isTraceEnabled())
+			log.trace( "removing statements from iterator" );
+		
 		while (statements.hasNext())
 			removedStatement(statements.nextStatement());
 	}
 
 	public void removedStatements(Model m)
 	{
+		if (log.isTraceEnabled())
+			log.trace( "removing statements from model" );
+		
 		removedStatements(m.listStatements());
 	}
 }
