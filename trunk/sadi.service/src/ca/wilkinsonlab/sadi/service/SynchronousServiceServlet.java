0facafc472b4b48b55ff16ba7558878d3c6f5468
elmccarthy <elmccarthy@e244ecbc-23bd-11de-9d52-39b120432c5d>
2009-06-17 19:54:01 +0000

    Reorganized service class hierarchy.

--- trunk/sadi.service/src/ca/wilkinsonlab/sadi/service/SynchronousServiceServlet.java
+++ trunk/sadi.service/src/ca/wilkinsonlab/sadi/service/SynchronousServiceServlet.java
@@ -8,12 +8,20 @@ import com.hp.hpl.jena.rdf.model.Resource;
  * This is the base class extended by synchronous SADI services.
  * @author Luke McCarthy
  */
-public abstract class SynchronousServiceServlet extends ServiceServlet
+public abstract class SynchronousServiceServlet extends ServiceServlet implements InputProcessor
 {
+	/* (non-Javadoc)
+	 * @see ca.wilkinsonlab.sadi.service.ServiceServlet#processInput(java.util.Map)
+	 */
 	@Override
 	public void processInput(Map<Resource, Resource> inputOutputMap)
 	{
 		for (Resource input: inputOutputMap.keySet())
 			getInputProcessor().processInput(input, inputOutputMap.get(input));
+	}
+	
+	protected InputProcessor getInputProcessor()
+	{
+		return this;
 	}
 }
