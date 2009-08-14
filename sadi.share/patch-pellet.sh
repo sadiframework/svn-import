b711dae07c000d96c1515275e9c9719aab3ca456
elmccarthy <elmccarthy@gmail.com>
2009-08-14 20:22:23 +0000

    Corrected patch-pellet.sh.

--- sadi.share/patch-pellet.sh
+++ sadi.share/patch-pellet.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+#
+# this script must be run from the sadi.share output directory
 
-find bin/org/mindswap/pellet -name *.class | xargs jar uvf ../sadi.libraries/sadi-0.1pre/pellet.jar
+find org/mindswap/pellet -name *.class | xargs jar uvf ../../sadi.libraries/sadi-0.1pre/pellet.jar
