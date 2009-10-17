0122cee3580f37542d198144c8a7bd9509811ce3
elmccarthy <elmccarthy@gmail.com>
2009-10-17 06:24:32 +0000

    Reorganized project to conform to Maven webapp archetype directory structure.

---
+++ sadi.examples/src/test/java/ca/wilkinsonlab/sadi/service/example/UniProtInfoServiceServletTest.java
@@ -0,0 +1,43 @@
+package ca.wilkinsonlab.sadi.service.example;
+
+import ca.wilkinsonlab.sadi.service.ServiceServlet;
+import ca.wilkinsonlab.sadi.service.ServiceServletTestBase;
+
+public class UniProtInfoServiceServletTest extends ServiceServletTestBase
+{
+	@Override
+	protected Object getInput()
+	{
+		return UniProtInfoServiceServletTest.class.getResourceAsStream("/uniprotInfo-input.rdf");
+	}
+
+	@Override
+	protected Object getExpectedOutput()
+	{
+		return UniProtInfoServiceServletTest.class.getResourceAsStream("/uniprotInfo-output.rdf");
+	}
+
+	@Override
+	protected String getInputURI()
+	{
+		return "http://purl.uniprot.org/uniprot/P12345";
+	}
+
+	@Override
+	protected String getServiceURI()
+	{
+		return "http://sadiframework.org/examples/uniprotInfo";
+	}
+
+	@Override
+	protected String getLocalServiceURL()
+	{
+		return "http://localhost:8080/sadi.examples/uniprotInfo";
+	}
+
+	@Override
+	protected ServiceServlet getServiceServletInstance()
+	{
+		return new UniProtInfoServiceServlet();
+	}
+}
