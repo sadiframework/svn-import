02549dcfd56ca2bef3c1612c5367db38fff9a7c2
elmccarthy <elmccarthy@e244ecbc-23bd-11de-9d52-39b120432c5d>
2009-11-11 02:31:03 +0000

    Moved common linear regression code to RegressionUtils.

--- trunk/sadi.examples/src/main/java/ca/wilkinsonlab/sadi/service/example/AsyncLinearRegressionServiceServlet.java
+++ trunk/sadi.examples/src/main/java/ca/wilkinsonlab/sadi/service/example/AsyncLinearRegressionServiceServlet.java
@@ -2,28 +2,15 @@ package ca.wilkinsonlab.sadi.service.example;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.commons.math.stat.regression.SimpleRegression;
 
 import ca.wilkinsonlab.sadi.service.simple.SimpleAsynchronousServiceServlet;
 
-import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.Resource;
-import com.hp.hpl.jena.rdf.model.ResourceFactory;
-import com.hp.hpl.jena.rdf.model.Statement;
-import com.hp.hpl.jena.rdf.model.StmtIterator;
 
 @SuppressWarnings("serial")
 public class AsyncLinearRegressionServiceServlet extends SimpleAsynchronousServiceServlet
 {
 	private static final Log log = LogFactory.getLog(AsyncLinearRegressionServiceServlet.class);
-
-	private final Property element = ResourceFactory.createProperty("http://sadiframework.org/examples/regression.owl#element");
-	private final Property x = ResourceFactory.createProperty("http://sadiframework.org/examples/regression.owl#x");
-	private final Property y = ResourceFactory.createProperty("http://sadiframework.org/examples/regression.owl#y");
-	private final Resource LinearRegressionModel = ResourceFactory.createResource("http://sadiframework.org/examples/regression.owl#LinearRegressionModel");
-	private final Property hasRegressionModel = ResourceFactory.createProperty("http://sadiframework.org/examples/regression.owl#hasRegressionModel");
-	private final Property slope = ResourceFactory.createProperty("http://sadiframework.org/examples/regression.owl#slope");
-	private final Property intercept = ResourceFactory.createProperty("http://sadiframework.org/examples/regression.owl#intercept");
 	
 	public AsyncLinearRegressionServiceServlet()
 	{
@@ -32,21 +19,7 @@ public class AsyncLinearRegressionServiceServlet extends SimpleAsynchronousServi
 	
 	public void processInput(Resource input, Resource output)
 	{
-		log.trace("processing input " + input);
-		SimpleRegression regressionModel = new SimpleRegression();
-		for (StmtIterator statements = input.listProperties(element); statements.hasNext(); ) {
-			Statement statement = statements.nextStatement();
-			Resource pair = statement.getResource();
-			log.trace("found list item " + pair);
-			regressionModel.addData(pair.getProperty(x).getDouble(), pair.getProperty(y).getDouble());
-		}
-		
-		double m = regressionModel.getSlope();
-		double x = regressionModel.getIntercept();
-		Resource linearRegressionModel = output.getModel().createResource(LinearRegressionModel);
-		linearRegressionModel.addLiteral(slope, m);
-		linearRegressionModel.addLiteral(intercept, x);
-		output.addProperty(hasRegressionModel, linearRegressionModel);
+		RegressionUtils.processInput(input, output);
 		
 		log.info("waiting 5 seconds to facilitate debugging");
 		try {
