7363e2cfa71afb0c4a78eb2b508853abf59d88bb
ed.kawas@gmail.com <ed.kawas@gmail.com@e244ecbc-23bd-11de-9d52-39b120432c5d>
2010-03-16 18:24:53 +0000

    expanded the import statements

--- trunk/sadi.protege/src/org/sadiframework/swing/SpringUtilities.java
+++ trunk/sadi.protege/src/org/sadiframework/swing/SpringUtilities.java
@@ -31,9 +31,12 @@
 
 package org.sadiframework.swing;
 
-import javax.swing.*;
+import java.awt.Component;
+import java.awt.Container;
+
+import javax.swing.Spring;
 import javax.swing.SpringLayout;
-import java.awt.*;
+
 
 /**
  * A 1.4 file that provides utility methods for
