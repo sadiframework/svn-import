5b4b0b52d80d076e0dcf623c74c7ed6e5f520b27
elmccarthy <elmccarthy@gmail.com>
2010-04-08 00:46:48 +0000

    Added testOntology to help diagnose failures in ServiceImplTest.

--- sadi.client/src/test/java/ca/wilkinsonlab/sadi/client/ServiceImplTest.java
+++ sadi.client/src/test/java/ca/wilkinsonlab/sadi/client/ServiceImplTest.java
@@ -1,5 +1,6 @@
 package ca.wilkinsonlab.sadi.client;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Collection;
@@ -10,9 +11,12 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import ca.wilkinsonlab.sadi.client.ServiceImpl;
 import ca.wilkinsonlab.sadi.common.SADIException;
+import ca.wilkinsonlab.sadi.utils.OwlUtils;
 
+import com.hp.hpl.jena.ontology.OntClass;
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.ontology.OntModelSpec;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Property;
@@ -24,9 +28,11 @@ public class ServiceImplTest
 	private static final Logger log = Logger.getLogger(ServiceImplTest.class);
 
 	private static final String DUMMY_SERVICE = "http://localhost/dummy";
-	private static final String DUMMY_INPUT_CLASS = "http://elmonline.ca/sw/explore.owl#ParentClass";
-	private static final String DUMMY_OUTPUT_CLASS = "http://elmonline.ca/sw/explore.owl#OtherClass";
-	private static final String DUMMY_PREDICATE = "http://elmonline.ca/sw/explore.owl#childProperty";
+	private static final String DUMMY_NS = "http://sadiframework.org/test/dummy.owl#";
+	private static final String DUMMY_INPUT_CLASS = DUMMY_NS + "InputClass";
+	private static final String DUMMY_OUTPUT_CLASS = DUMMY_NS + "OutputClass";
+	private static final String DUMMY_INPUT_PREDICATE = DUMMY_NS + "inputProperty";
+	private static final String DUMMY_OUTPUT_PREDICATE = DUMMY_NS + "outputProperty";
 	private static final String DIRECT_INSTANCE = "http://localhost/dummy/directInstance";
 	private static final String INFERRED_INSTANCE = "http://localhost/dummy/inferredInstance";
 	private static final String DIRECT_INSTANCE_NO_PROPERTY = "http://localhost/dummy/directInstanceNoProperty";
@@ -64,20 +70,42 @@ public class ServiceImplTest
 	{
 		Model model = ModelFactory.createDefaultModel();
 		Resource inputClass = model.createResource(DUMMY_INPUT_CLASS);
-		Property predicate = model.createProperty(DUMMY_PREDICATE);
+		Property inputProperty = model.createProperty(DUMMY_INPUT_PREDICATE);
+		Resource object = model.createResource();
+		
 		Resource directInstance = model.createResource(DIRECT_INSTANCE, inputClass);
-		directInstance.addProperty(predicate, directInstance);
+		directInstance.addProperty(inputProperty, object);
+		
 		Resource inferredInstance = model.createResource(INFERRED_INSTANCE);
-		inferredInstance.addProperty(predicate, directInstance);
+		inferredInstance.addProperty(inputProperty, object);
+		
 		model.createResource(DIRECT_INSTANCE_NO_PROPERTY, inputClass);
 		
 		return model;
 	}
 	
 	@Test
+	public void testOntology() throws Exception
+	{
+		OntModel model = ModelFactory.createOntologyModel( OntModelSpec.OWL_MEM_MICRO_RULE_INF );
+//		model.read(DUMMY_NS);
+//		OntClass inputClass = model.getOntClass(DUMMY_INPUT_CLASS);
+		OntClass inputClass = OwlUtils.getOntClassWithLoad(model, DUMMY_INPUT_CLASS);
+		assertNotNull(String.format("input class %s is undefined", DUMMY_INPUT_CLASS), inputClass);
+//		OntClass outputClass = model.getOntClass(DUMMY_OUTPUT_CLASS);
+		OntClass outputClass = OwlUtils.getOntClassWithLoad(model, DUMMY_OUTPUT_CLASS);
+		assertNotNull(String.format("output class %s is undefined", DUMMY_OUTPUT_CLASS), outputClass);
+		model.add(inputModel);
+		assertTrue(String.format("direct instance %s does not have type %s", DIRECT_INSTANCE, DUMMY_INPUT_CLASS),
+				model.getIndividual(DIRECT_INSTANCE).hasOntClass(inputClass));
+		assertTrue(String.format("indirect instance %s does not have type %s", INFERRED_INSTANCE, DUMMY_INPUT_CLASS),
+				model.getIndividual(INFERRED_INSTANCE).hasOntClass(inputClass));
+	}
+	
+	@Test
 	public void testGetPredicates() throws Exception
 	{
-		String[] expectedPredicates = new String[]{ DUMMY_PREDICATE };
+		String[] expectedPredicates = new String[]{ DUMMY_OUTPUT_PREDICATE };
 		Collection<String> actualPredicates = service.getPredicates();
 		for (String expected: expectedPredicates) {
 			assertTrue("service does not provide expected predicate " + expected, actualPredicates.contains(expected));
