2c889c3b6b77e52f42956dbb2b5785b96421759e
ben.vvalk <ben.vvalk@gmail.com>
2010-05-07 23:40:11 +0000

    update adaptive query planner to handle unresolvable predicates

--- sadi.share/src/test/java/ca/wilkinsonlab/sadi/share/QueryPatternComparatorTest.java
+++ sadi.share/src/test/java/ca/wilkinsonlab/sadi/share/QueryPatternComparatorTest.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,8 +21,15 @@ import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.Resource;
 
+import ca.wilkinsonlab.sadi.client.MultiRegistry;
+import ca.wilkinsonlab.sadi.client.Registry;
+import ca.wilkinsonlab.sadi.client.Service;
+import ca.wilkinsonlab.sadi.client.ServiceBase;
+import ca.wilkinsonlab.sadi.client.ServiceInvocationException;
 import ca.wilkinsonlab.sadi.stats.PredicateStatsDB;
 import ca.wilkinsonlab.sadi.utils.SPARQLStringUtils;
+
+import org.apache.log4j.Logger;
 
 /*
  * These tests are for QueryPatternComparator, an inner 
@@ -34,32 +44,34 @@ import ca.wilkinsonlab.sadi.utils.SPARQLStringUtils;
 
 public class QueryPatternComparatorTest 
 {
+	protected static final Logger log = Logger.getLogger(QueryPatternComparatorTest.class);
+	
 	protected static final String NODE_PREFIX = "http://dummynode/";
 	protected static final String PREDICATE_PREFIX = "http://dummypredicate/";
 
 	// predicate A is equivalent to predicate C
 	
-	protected static final String PREDICATE_A = PREDICATE_PREFIX + "A"; 
+	protected static final String INVERSE_SUFFIX = "-inverse";
+	
+	protected static final String PREDICATE_A = PREDICATE_PREFIX + "A";
+	protected static final String PREDICATE_A_INVERSE = PREDICATE_A + INVERSE_SUFFIX;
 	protected static final String PREDICATE_B = PREDICATE_PREFIX + "B";
+	protected static final String PREDICATE_B_INVERSE = PREDICATE_B + INVERSE_SUFFIX;
 	protected static final String PREDICATE_C = PREDICATE_PREFIX + "C";
+	protected static final String PREDICATE_C_INVERSE = PREDICATE_C + INVERSE_SUFFIX;
 	
 	protected static final String NO_STATS_PREDICATE_D = PREDICATE_PREFIX + "D"; 
+	protected static final String NO_STATS_PREDICATE_D_INVERSE = NO_STATS_PREDICATE_D + INVERSE_SUFFIX;
 	protected static final String NO_STATS_PREDICATE_E = PREDICATE_PREFIX + "E"; 
+	protected static final String NO_STATS_PREDICATE_E_INVERSE = NO_STATS_PREDICATE_E + INVERSE_SUFFIX;
+	
+	protected static final String UNRESOLVABLE_PREDICATE_F = PREDICATE_PREFIX + "F";
+	protected static final String PREDICATE_F_INVERSE = UNRESOLVABLE_PREDICATE_F + INVERSE_SUFFIX;
 	
 	protected static final String NODE_A = NODE_PREFIX + "A";
 	protected static final String NODE_B = NODE_PREFIX + "B";
 	protected static final String NODE_C = NODE_PREFIX + "C";
 	protected static final String NODE_D = NODE_PREFIX + "D";
-	
-	protected Triple forwardPatternWithStats;
-	protected Triple reversePatternWithStats;
-	protected Triple forwardPatternWithoutStats;
-	protected Triple reversePatternWithoutStats;
-	protected Triple unboundPredicatePattern;
-	protected Triple forwardPatternWithMultipleSubjectBindings;
-	protected Triple forwardPatternWithMultipleSubjectBindings2;
-	protected Triple forwardPatternWithMultipleSubjectBindings3;
-	protected Triple reversePatternWithMultiplePredicateBindings;
 	
 	protected static MockKnowledgeBase mockKB;
 	
@@ -70,8 +82,16 @@ public class QueryPatternComparatorTest
 		OntProperty pA = reasoningModel.createOntProperty(PREDICATE_A);
 		OntProperty pB = reasoningModel.createOntProperty(PREDICATE_B);
 		OntProperty pC = reasoningModel.createOntProperty(PREDICATE_C);
-		reasoningModel.createOntProperty(NO_STATS_PREDICATE_D);
-		reasoningModel.createOntProperty(NO_STATS_PREDICATE_E);
+		OntProperty pD = reasoningModel.createOntProperty(NO_STATS_PREDICATE_D);
+		OntProperty pE = reasoningModel.createOntProperty(NO_STATS_PREDICATE_E);
+		OntProperty pF = reasoningModel.createOntProperty(UNRESOLVABLE_PREDICATE_F);
+		
+		pA.addInverseOf(reasoningModel.createOntProperty(PREDICATE_A_INVERSE));
+		pB.addInverseOf(reasoningModel.createOntProperty(PREDICATE_B_INVERSE));
+		pC.addInverseOf(reasoningModel.createOntProperty(PREDICATE_C_INVERSE));
+		pD.addInverseOf(reasoningModel.createOntProperty(NO_STATS_PREDICATE_D_INVERSE));
+		pE.addInverseOf(reasoningModel.createOntProperty(NO_STATS_PREDICATE_E_INVERSE));
+		pF.addInverseOf(reasoningModel.createOntProperty(PREDICATE_F_INVERSE));
 		
 		pA.addEquivalentProperty(pC);
 		
@@ -86,7 +106,7 @@ public class QueryPatternComparatorTest
 		mockKB = new MockKnowledgeBase(reasoningModel);
 		
 		/* we need predicate variables for our tests */
-		mockKB.allowPredicateVariables = true;
+		mockKB.setAllowPredicateVariables(true);
 		
 		/* assign bindings to some variables, without accessing the internals of SHAREKnowledgeBase */
 		mockKB.executeQuery(SPARQLStringUtils.strFromTemplate("SELECT * WHERE { ?var1 %u% ?var2 }", PREDICATE_B));
@@ -154,6 +174,44 @@ public class QueryPatternComparatorTest
 	}
 
 	@Test
+	public void testCompareWithUnresolvablePattern()
+	{
+		Triple pattern1 = createTriple("?unbound", "?unbound", "?unbound");
+		Triple pattern2 = createTriple(NODE_A, PREDICATE_A, "?unbound");
+		assertTrue(mockKB.comparator.compare(pattern1, pattern2) == 1);
+	}
+
+	@Test
+	public void testCompareWithUnresolvablePattern2()
+	{
+		Triple pattern1 = createTriple(NODE_A, UNRESOLVABLE_PREDICATE_F, "?unbound");
+		Triple pattern2 = createTriple(NODE_A, PREDICATE_A, "?unbound");
+		assertTrue(mockKB.comparator.compare(pattern1, pattern2) == 1);
+	}
+	
+	@Test
+	public void testCompareWithUnresolvablePattern3()
+	{
+		/*
+		 * At the current time, all unresolvable patterns are considered equal.
+		 */
+		Triple pattern1 = createTriple(NODE_A, UNRESOLVABLE_PREDICATE_F, "?unbound");
+		Triple pattern2 = createTriple("?unbound", PREDICATE_A, "?unbound");
+		assertTrue(mockKB.comparator.compare(pattern1, pattern2) == 0);
+	}
+
+	@Test
+	public void testCompareWithUnresolvablePattern4()
+	{
+		/*
+		 * An expensive pattern should be preferred to an unresolvable pattern.
+		 */
+		Triple pattern1 = createTriple(NODE_A, "?unbound", "?unbound");
+		Triple pattern2 = createTriple("?unbound", PREDICATE_A, "?unbound");
+		assertTrue(mockKB.comparator.compare(pattern1, pattern2) == -1);
+	}
+	
+	@Test
 	public void testBestDirectionIsForwardWithStats()
 	{
 		/*
@@ -174,6 +232,14 @@ public class QueryPatternComparatorTest
 		 * is best).  
 		 */
 		Triple pattern = createTriple("?var2", "?unbound", "?var1");
+		assertFalse(mockKB.comparator.bestDirectionIsForward(pattern));
+	}
+	
+	@Test
+	public void testBestDirectionIsForwardWithUnresolvablePredicate()
+	{
+		/* predicate F is only resolvable in the reverse direction */
+		Triple pattern = createTriple("?var1", UNRESOLVABLE_PREDICATE_F, "?var2");
 		assertFalse(mockKB.comparator.bestDirectionIsForward(pattern));
 	}
 	
@@ -223,13 +289,113 @@ public class QueryPatternComparatorTest
 
 	protected class MockKnowledgeBase extends SHAREKnowledgeBase
 	{
+		private MultiRegistry mockRegistry;
+		private PredicateStatsDB mockStatsDB;
+		
 		public QueryPatternComparator comparator = new QueryPatternComparator();
 		
 		public MockKnowledgeBase(OntModel reasoningModel) throws IOException
 		{
 			super(reasoningModel, true);
-			this.statsDB = new MockPredicateStatsDB();
+			this.mockStatsDB = new MockPredicateStatsDB();
+			this.mockRegistry = new MockMultiRegistry();
 		}
+		
+		@Override 
+		protected MultiRegistry getRegistry() {
+			return mockRegistry;
+		}
+		
+		@Override
+		protected PredicateStatsDB getStatsDB() {
+			return mockStatsDB;
+		}
+		
+	}
+
+	/**
+	 * A mock version of the MultiRegistry.
+	 * 
+	 * The QueryPatternComparator must take into account whether
+	 * a given predicate is resolvable (i.e. maps to any services)
+	 * in the forward and/or reverse direction, and it uses
+	 * the MultiRegistry to determine this. 
+	 * 
+	 * Thus we need a fake implementation of "findServicesByPredicate".
+	 * (All the other methods of Registry are unused.) 
+	 * 
+	 * I could define this class more briefly as an extension of
+	 * RegistryBase, but I think it's better if this object doesn't have any
+	 * "real" code in it. 
+	 */
+	protected class MockMultiRegistry extends MultiRegistry
+	{
+		Map<String, Boolean> isResolvable; 
+		
+		public MockMultiRegistry() throws IOException
+		{
+			super(new ArrayList<Registry>());
+			
+			isResolvable = new HashMap<String, Boolean>();
+
+			isResolvable.put(PREDICATE_A, true);
+			isResolvable.put(PREDICATE_A_INVERSE, true);
+			isResolvable.put(PREDICATE_B, true);
+			isResolvable.put(PREDICATE_B_INVERSE, true);
+			isResolvable.put(PREDICATE_C, true);
+			isResolvable.put(PREDICATE_C_INVERSE, true);
+			isResolvable.put(NO_STATS_PREDICATE_D, true);
+			isResolvable.put(NO_STATS_PREDICATE_D_INVERSE, true);
+			isResolvable.put(NO_STATS_PREDICATE_E, true);
+			isResolvable.put(NO_STATS_PREDICATE_E_INVERSE, true);
+			isResolvable.put(UNRESOLVABLE_PREDICATE_F, false);
+			isResolvable.put(PREDICATE_F_INVERSE, true);
+		}
+		
+		@Override
+		public Collection<Service> findServicesByPredicate(String predicate) 
+		{
+			if(isResolvable.containsKey(predicate) && (isResolvable.get(predicate) == true)) {
+				return Collections.singleton((Service)(new DummyService()));
+			}
+			
+			return new ArrayList<Service>();
+		}
+
+		protected class DummyService extends ServiceBase {
+
+			@Override
+			public String getDescription() {
+				throw new UnsupportedOperationException();
+			}
+
+			@Override
+			public String getInputClassURI() {
+				throw new UnsupportedOperationException();
+			}
+
+			@Override
+			public String getName() {
+				throw new UnsupportedOperationException();
+			}
+
+			@Override
+			public String getOutputClassURI() {
+				throw new UnsupportedOperationException();
+			}
+
+			@Override
+			public Collection<Triple> invokeService(Collection<Resource> inputNodes) throws ServiceInvocationException {
+				throw new UnsupportedOperationException();
+			}
+
+			@Override
+			public boolean isInputInstance(Resource resource) {
+				throw new UnsupportedOperationException();
+			}
+			
+		}
+
 	}
 	
 	protected class MockPredicateStatsDB extends PredicateStatsDB
