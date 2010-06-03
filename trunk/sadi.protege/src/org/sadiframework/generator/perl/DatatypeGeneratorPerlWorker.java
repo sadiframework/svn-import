48f7429f23a37d02100520efef865f50ffb8456b
ed.kawas@gmail.com <ed.kawas@gmail.com@e244ecbc-23bd-11de-9d52-39b120432c5d>
2010-06-03 21:15:06 +0000

    properties now come from SADIProperties.java

--- trunk/sadi.protege/src/org/sadiframework/generator/perl/DatatypeGeneratorPerlWorker.java
+++ trunk/sadi.protege/src/org/sadiframework/generator/perl/DatatypeGeneratorPerlWorker.java
@@ -9,8 +9,8 @@ import java.io.IOException;
 
 import org.jdesktop.swingworker.SwingWorker;
 import org.sadiframework.editor.views.SADIPreferencePanel;
-import org.sadiframework.editor.views.SadiServiceGeneratorView;
 import org.sadiframework.preferences.PreferenceManager;
+import org.sadiframework.properties.SADIProperties;
 
 /**
  * @author Eddie
@@ -25,12 +25,12 @@ public class DatatypeGeneratorPerlWorker extends SwingWorker<String, Object> {
 
         this.filename = filename == null ? "" : filename;
         // listen for changes to the DO_SERVICE_GENERATION preference
-        manager.addPropertyChangeListener(SadiServiceGeneratorView.DO_DATATYPE_GENERATION,
+        manager.addPropertyChangeListener(SADIProperties.DO_PERL_DATATYPE_GENERATION,
                 new PropertyChangeListener() {
                     public void propertyChange(PropertyChangeEvent evt) {
                         // cancel our task
                         if (!manager.getBooleanPreference(
-                                SadiServiceGeneratorView.DO_DATATYPE_GENERATION, false)) {
+                                SADIProperties.DO_PERL_DATATYPE_GENERATION, false)) {
                             if (!isCancelled() || !isDone()) {
                                 cancel(true);
                             }
@@ -56,9 +56,9 @@ public class DatatypeGeneratorPerlWorker extends SwingWorker<String, Object> {
         try {
             str = gen.generateDatatypes(filename);
         } catch (IOException ioe) {
-            manager.saveBooleanPreference(SadiServiceGeneratorView.DO_DATATYPE_GENERATION, false);
+            manager.saveBooleanPreference(SADIProperties.DO_PERL_DATATYPE_GENERATION, false);
         } catch (InterruptedException ie) {
-            manager.saveBooleanPreference(SadiServiceGeneratorView.DO_DATATYPE_GENERATION, false);
+            manager.saveBooleanPreference(SADIProperties.DO_PERL_DATATYPE_GENERATION, false);
         }
         return str;
     }
@@ -70,7 +70,7 @@ public class DatatypeGeneratorPerlWorker extends SwingWorker<String, Object> {
      */
     protected void done() {
         super.done();
-        manager.saveBooleanPreference(SadiServiceGeneratorView.DO_DATATYPE_GENERATION, false);
+        manager.saveBooleanPreference(SADIProperties.DO_PERL_DATATYPE_GENERATION, false);
     }
 
 }
