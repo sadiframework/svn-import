ab9efc5dd8745e0534e5e5fba1c0a05e7f5a2fb1
ed.kawas@gmail.com <ed.kawas@gmail.com@e244ecbc-23bd-11de-9d52-39b120432c5d>
2010-06-03 21:03:25 +0000

    removed the java section of preferences view because there is nothing to set just yet

--- trunk/sadi.protege/src/org/sadiframework/editor/views/SADIPreferencePanel.java
+++ trunk/sadi.protege/src/org/sadiframework/editor/views/SADIPreferencePanel.java
@@ -32,22 +32,33 @@ public class SADIPreferencePanel extends org.protege.editor.core.ui.preferences.
     private final ResourceBundle bundle = ResourceBundle
             .getBundle("org.sadiframework.utils.i18n.EditorResourceBundle");
 
-    public static String PERL_PATH = "perl-exe-path";
-    public static String PERL_5LIB_DIR = "perl-5lib-dir";
-    public static String PERL_SADI_SCRIPTS_DIR = "sadises-scripts-dir";
+    // Perl SADI variables
+    public final static String PERL_PATH = "perl-exe-path";
+    public final static String PERL_5LIB_DIR = "perl-5lib-dir";
+    public final static String PERL_SADI_SCRIPTS_DIR = "sadises-scripts-dir";
 
     private String TMP_PERL_PATH = "perl-exe-path-tmp";
     private String TMP_PERL_5LIB_DIR = "perl-5lib-dir-tmp";
     private String TMP_PERL_SADI_SCRIPTS_DIR = "sadises-scripts-dir-tmp";
 
     private String[] PERL_SADI_PREFERENCE_KEYS = { PERL_PATH, PERL_5LIB_DIR, PERL_SADI_SCRIPTS_DIR };
-    private String[] TMP_PERL_SADI_PREFERENCE_KEYS = { TMP_PERL_PATH, TMP_PERL_5LIB_DIR,
-            TMP_PERL_SADI_SCRIPTS_DIR };
-    private String[] PERL_SADI_LABELS = { "Perl Path: ", "Perl LIB Directories: ",
-            "Perl SADI Scripts Directory: " };
-    private String[] PERL_SADI_HELP = { bundle.getString("preference_help_perl_exe"),
-            bundle.getString("preference_help_perl_5lib"),
-            bundle.getString("preference_help_perl_sadi_scripts") };
+    private String[] TMP_PERL_SADI_PREFERENCE_KEYS = { TMP_PERL_PATH, TMP_PERL_5LIB_DIR, TMP_PERL_SADI_SCRIPTS_DIR };
+    
+    private String[] PERL_SADI_LABELS = { "Perl Path: ", "Perl LIB Directories: ", "Perl SADI Scripts Directory: " };
+    private String[] PERL_SADI_HELP = { 
+                bundle.getString("preference_help_perl_exe"),
+                bundle.getString("preference_help_perl_5lib"),
+                bundle.getString("preference_help_perl_sadi_scripts")
+            };
+    
+    // Java variables
+    
+    private String[] JAVA_SADI_PREFERENCE_KEYS = { };
+    private String[] TMP_JAVA_SADI_PREFERENCE_KEYS = { };
+    
+    private String[] JAVA_SADI_LABELS = { };
+    private String[] JAVA_SADI_HELP = { };
+    
 
     @Override
     public void applyChanges() {
@@ -56,6 +67,10 @@ public class SADIPreferencePanel extends org.protege.editor.core.ui.preferences.
         for (int i = 0; i < TMP_PERL_SADI_PREFERENCE_KEYS.length; i++) {
             manager.savePreference(PERL_SADI_PREFERENCE_KEYS[i], manager.getPreference(
                     TMP_PERL_SADI_PREFERENCE_KEYS[i], ""));
+        }
+        for (int i = 0; i < TMP_JAVA_SADI_PREFERENCE_KEYS.length; i++) {
+            manager.savePreference(JAVA_SADI_PREFERENCE_KEYS[i], manager.getPreference(
+                    TMP_JAVA_SADI_PREFERENCE_KEYS[i], ""));
         }
     }
 
@@ -67,8 +82,8 @@ public class SADIPreferencePanel extends org.protege.editor.core.ui.preferences.
     private Component getPreferencesPanel() {
         // Create and populate the perl panel. (reset the values to saved
         // values)
-        JPanel perlPanel = new JPanel(new GridBagLayout());
-        perlPanel.setBorder(BorderFactory.createTitledBorder("Perl SADI"));
+        JPanel preferencesPanel = new JPanel(new GridBagLayout());
+        preferencesPanel.setBorder(BorderFactory.createTitledBorder("Perl SADI"));
         for (int i = 0; i < TMP_PERL_SADI_PREFERENCE_KEYS.length; i++) {
             // reset preferences
             manager.savePreference(TMP_PERL_SADI_PREFERENCE_KEYS[i], manager.getPreference(
@@ -80,12 +95,12 @@ public class SADIPreferencePanel extends org.protege.editor.core.ui.preferences.
             JButton hBtn = UIUtils.createHelpButton(PERL_SADI_HELP[i]);
 
             UIUtils
-                    .addComponent(perlPanel, label, 0, i, 1, 1, UIUtils.WEST, UIUtils.NONE, 0.0,
+                    .addComponent(preferencesPanel, label, 0, i, 1, 1, UIUtils.WEST, UIUtils.NONE, 0.0,
                             0.0);
             UIUtils
-                    .addComponent(perlPanel, field, 1, i, 3, 1, UIUtils.WEST, UIUtils.HORI, 0.0,
+                    .addComponent(preferencesPanel, field, 1, i, 3, 1, UIUtils.WEST, UIUtils.HORI, 0.0,
                             0.0);
-            UIUtils.addComponent(perlPanel, hBtn, 4, i, 1, 1, UIUtils.WEST, UIUtils.NONE, 0.0, 0.0);
+            UIUtils.addComponent(preferencesPanel, hBtn, 4, i, 1, 1, UIUtils.WEST, UIUtils.NONE, 0.0, 0.0);
         }
         LinkLabel l = new LinkLabel("SADI Homepage", new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -94,15 +109,35 @@ public class SADIPreferencePanel extends org.protege.editor.core.ui.preferences.
         });
         l.setHorizontalAlignment(JLabel.LEADING);
 
-        // TODO create the java panel
+        // create the java panel
         JPanel javaPanel = new JPanel(new GridBagLayout());
         javaPanel.setBorder(BorderFactory.createTitledBorder("jSADI"));
+        
+        for (int i = 0; i < TMP_JAVA_SADI_PREFERENCE_KEYS.length; i++) {
+            // reset preferences
+            manager.savePreference(JAVA_SADI_PREFERENCE_KEYS[i], manager.getPreference(
+                    JAVA_SADI_PREFERENCE_KEYS[i], ""));
+            // create the interface
+            JLabel label = new JLabel(JAVA_SADI_LABELS[i]);
+            JTextFieldWithHistory field = new JTextFieldWithHistory(25,
+                    TMP_JAVA_SADI_PREFERENCE_KEYS[i]);
+            JButton hBtn = UIUtils.createHelpButton(JAVA_SADI_HELP[i]);
+
+            UIUtils
+                    .addComponent(javaPanel, label, 0, i, 1, 1, UIUtils.WEST, UIUtils.NONE, 0.0,
+                            0.0);
+            UIUtils
+                    .addComponent(javaPanel, field, 1, i, 3, 1, UIUtils.WEST, UIUtils.HORI, 0.0,
+                            0.0);
+            UIUtils.addComponent(javaPanel, hBtn, 4, i, 1, 1, UIUtils.WEST, UIUtils.NONE, 0.0, 0.0);
+        }
+        
 
         // create the main panel
         JPanel panel = new JPanel(new GridBagLayout());
-        UIUtils.addComponent(panel, perlPanel, 0, 0, 1, 1, UIUtils.NWEST, UIUtils.HORI, 0.0, 0.0);
-        UIUtils.addComponent(panel, javaPanel, 0, 1, 1, 1, UIUtils.NWEST, UIUtils.HORI, 0.0, 0.0);
-        UIUtils.addComponent(panel, l, 0, 2, 1, 1, UIUtils.SWEST, UIUtils.HORI, 0.0, 0.0);
+        UIUtils.addComponent(panel, preferencesPanel, 0, 0, 1, 1, UIUtils.NWEST, UIUtils.HORI, 0.0, 0.0);
+        //UIUtils.addComponent(panel, javaPanel, 0, 1, 1, 1, UIUtils.NWEST, UIUtils.HORI, 0.0, 0.0);
+        UIUtils.addComponent(panel, l, 0, 2, 1, 1, UIUtils.SWEST, UIUtils.NONE, 0.0, 0.0);
 
         return panel;
     }
