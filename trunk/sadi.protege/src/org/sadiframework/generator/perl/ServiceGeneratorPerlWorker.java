0bbec563dc67e3e453fff751877539c42fef4c16
ed.kawas@gmail.com <ed.kawas@gmail.com@e244ecbc-23bd-11de-9d52-39b120432c5d>
2010-06-03 21:15:39 +0000

    some formattting changes. also properties now come from SADIProperties.java

--- trunk/sadi.protege/src/org/sadiframework/generator/perl/ServiceGeneratorPerlWorker.java
+++ trunk/sadi.protege/src/org/sadiframework/generator/perl/ServiceGeneratorPerlWorker.java
@@ -9,57 +9,62 @@ import java.io.IOException;
 
 import org.jdesktop.swingworker.SwingWorker;
 import org.sadiframework.editor.views.SADIPreferencePanel;
-import org.sadiframework.editor.views.SadiServiceGeneratorView;
 import org.sadiframework.preferences.PreferenceManager;
+import org.sadiframework.properties.SADIProperties;
 
 /**
  * @author Eddie
- *
+ * 
  */
 public class ServiceGeneratorPerlWorker extends SwingWorker<String, Object> {
 
     private PreferenceManager manager = PreferenceManager.newInstance();
-    
+
     public ServiceGeneratorPerlWorker() {
         // listen for changes to the DO_SERVICE_GENERATION preference
-        manager.addPropertyChangeListener(SadiServiceGeneratorView.DO_SERVICE_GENERATION, new PropertyChangeListener() { 
-            public void propertyChange(PropertyChangeEvent evt) {
-                // cancel our task
-                if (!manager.getBooleanPreference(SadiServiceGeneratorView.DO_SERVICE_GENERATION, false)) {
-                    if (!isCancelled() || !isDone()) {
-                        cancel(true);
+        manager.addPropertyChangeListener(SADIProperties.DO_PERL_SERVICE_GENERATION,
+                new PropertyChangeListener() {
+                    public void propertyChange(PropertyChangeEvent evt) {
+                        // cancel our task
+                        if (!manager.getBooleanPreference(
+                                SADIProperties.DO_PERL_SERVICE_GENERATION, false)) {
+                            if (!isCancelled() || !isDone()) {
+                                cancel(true);
+                            }
+                        }
                     }
-                }
-            }
-        });
+                });
     }
-    
-    /* (non-Javadoc)
+
+    /*
+     * (non-Javadoc)
+     * 
      * @see org.jdesktop.swingworker.SwingWorker#doInBackground()
      */
+    // PRE: all applicable fields needed are correct (i.e. name, definition file exists, etc)
+    // POST: unless cancelled, this will 
     protected String doInBackground() throws Exception {
         String perl = manager.getPreference(SADIPreferencePanel.PERL_PATH, "");
         String libs = manager.getPreference(SADIPreferencePanel.PERL_5LIB_DIR, "");
-        String scriptDir = manager.getPreference(
-                SADIPreferencePanel.PERL_SADI_SCRIPTS_DIR, "");
-        boolean isAsync = manager.getBooleanPreference(SadiServiceGeneratorView.PERL_GENERATOR_SERVICE_ASYNC, true);
-        String name = manager.getPreference(SadiServiceGeneratorView.PERL_GENERATOR_SERVICE_NAME, "");
-
-        // TODO make sure that the service name is not blank!
+        String scriptDir = manager.getPreference(SADIPreferencePanel.PERL_SADI_SCRIPTS_DIR, "");
+        boolean isAsync = manager
+                .getBooleanPreference(SADIProperties.GENERATOR_SERVICE_ASYNC, true);
+        String name = manager.getPreference(SADIProperties.GENERATOR_SERVICE_NAME, "");
         Generator gen = new Generator(perl, libs, scriptDir);
         String str = "";
         try {
             str = gen.generateService(name, isAsync);
         } catch (IOException ioe) {
-            manager.saveBooleanPreference(SadiServiceGeneratorView.DO_SERVICE_GENERATION, false);
+            manager.saveBooleanPreference(SADIProperties.DO_PERL_SERVICE_GENERATION, false);
         } catch (InterruptedException ie) {
-            manager.saveBooleanPreference(SadiServiceGeneratorView.DO_SERVICE_GENERATION, false);
+            manager.saveBooleanPreference(SADIProperties.DO_PERL_SERVICE_GENERATION, false);
         }
         return str;
     }
+
     protected void done() {
         super.done();
-        manager.saveBooleanPreference(SadiServiceGeneratorView.DO_SERVICE_GENERATION, false);
+        manager.saveBooleanPreference(SADIProperties.DO_PERL_SERVICE_GENERATION, false);
     }
 
 }
