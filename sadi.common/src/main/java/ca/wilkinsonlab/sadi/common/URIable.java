cb2964d7bcd21c855a2e3847c0e87e21d866f053
elmccarthy <elmccarthy@gmail.com>
2010-07-01 02:56:16 +0000

    Added URIable interface.

---
+++ sadi.common/src/main/java/ca/wilkinsonlab/sadi/common/URIable.java
@@ -0,0 +1,11 @@
+package ca.wilkinsonlab.sadi.common;
+
+/**
+ * An interface for objects that have a URI.
+ * @author Luke McCarthy
+ */
+public interface URIable
+{
+	public String getURI();
+	public void setURI(String uri);
+}
