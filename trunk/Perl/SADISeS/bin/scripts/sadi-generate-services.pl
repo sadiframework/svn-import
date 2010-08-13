06dd60318fba1beef5ac151c7c7c92e8b69ca085
ed.kawas@gmail.com <ed.kawas@gmail.com@e244ecbc-23bd-11de-9d52-39b120432c5d>
2010-08-13 15:09:39 +0000

    added an option to additionally generate OWL2Perl datatypes for the output classes of the services

--- trunk/Perl/SADISeS/bin/scripts/sadi-generate-services.pl
+++ trunk/Perl/SADISeS/bin/scripts/sadi-generate-services.pl
@@ -8,14 +8,15 @@
 
 # some command-line options
 use Getopt::Std;
-use vars qw/ $opt_h $opt_A $opt_B $opt_d $opt_v  $opt_s $opt_b $opt_F $opt_S $opt_D $opt_T $opt_g/;
-getopts('hdvsbFSADBT');
+use vars qw/ $opt_h $opt_A $opt_B $opt_d $opt_v  $opt_s $opt_b $opt_F $opt_S $opt_D $opt_T $opt_g /;
+getopts('hdvsbFSADBTg');
 
 # usage
 if ( $opt_h or @ARGV == 0 ) {
 	print STDOUT <<'END_OF_USAGE';
 Generate Services.
 Usage: [-vds] [-b|S|A|D] service-name [service-name...]
+       [-vds] [-b|S|A] -g service-name [service-name...]
 
     All parameters for generating services are taken from the 
    'sadi-service.cfg' configuration file.
@@ -34,7 +35,8 @@ Usage: [-vds] [-b|S|A|D] service-name [service-name...]
     -T ... generate a unit test for your service that you can fill in
 
     -g ... generate OWL2Perl modules for your services output OWL class 
-           if you havae already created a service definition.
+           if you have already created a service definition. This operation
+           can take several minutes.
 
     If none of {-b,-S} given, it generates/show implementation (not a base) of 
     service[s].
@@ -56,6 +58,9 @@ END_OF_USAGE
 
 use strict;
 
+use FindBin qw( $Bin );
+use lib $Bin;
+use SADI::Utils;
 use SADI::Base;
 use SADI::Generators::GenServices;
 
@@ -128,6 +133,14 @@ if ($opt_s) {
 								  outcode       => \$code
 		);
 	}
+	if ($opt_g) {
+		my $service_instances = $generator->read_services(@ARGV);
+		my @urls;
+		foreach (@$service_instances) {
+			push @urls, $_->OutputClass;
+		}
+		generate_datatypes(urls => [@urls], outcode => \$code);
+	}
 	say $code;
 } else {
 	# generate code for a file
@@ -176,7 +189,66 @@ if ($opt_s) {
 		$generator->generate_cgi( service_names => [@ARGV],
 								  force_over    => $opt_F );
 	}
+	if ($opt_g) {
+        my $service_instances = $generator->read_services(@ARGV);
+        my @urls;
+        foreach (@$service_instances) {
+            push @urls, $_->OutputClass;
+        }
+        generate_datatypes(urls => [@urls]);
+    }
 }
 say 'Done.';
+
+sub generate_datatypes {
+    my %args = ( @_ );
+    return unless keys %args >= 1;
+    require OWL2Perl;
+    my $owl2perl = OWL2Perl->new();
+    
+    # set the output dir unless we are outputting to STDOUT
+    unless ($opt_s) {
+    
+        # set the outdir
+        $owl2perl->outdir( $SADICFG::GENERATORS_OUTDIR
+                           || SADI::Utils->find_file( $Bin, 'generated' ) );
+    
+        # tell people where the output is going
+        say sprintf( "Datatype output is going to %s\n", $owl2perl->outdir() );
+    }
+    
+    # set whether or not we follow imports
+    $owl2perl->follow_imports( 1 );
+
+    # set whether or not we overwrite files
+    $owl2perl->force( $opt_F ? 1 : 0 );
+    # owl_urls contain all owl files (as urls or file paths) to be parsed
+    # base_uris will hold the base_uris
+    my ( @owl_urls, @base_uris );
+    my $counter = -1;
+    my @urls = @ {$args{urls}};
+    
+    foreach my $arg (@urls) {
+        say "Processing OWL file: $arg\n";
+        my $base_uri = undef;
+        
+        # add the url and base_uri to our arrays
+        $counter++;
+        $owl_urls[$counter] = $arg;
+        $base_uris[$counter] = $base_uri;
+    }
+    say('Aggregating ontologies ...') if $opt_v;
+    my $ontology = $owl2perl->process_owl(\@owl_urls, \@base_uris);
+    
+    # generate the CODE
+    say('Generating PERL modules from the OWL documents ...') if $opt_v;
+    if ($opt_s) {
+        my $code = $args{outcode};
+        $owl2perl->generate_datatypes($ontology, $code);
+    } else {
+        $owl2perl->generate_datatypes($ontology);
+    }
+    
+}
 
 __END__
