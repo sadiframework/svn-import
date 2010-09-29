79c939ef3aadc55dc4fd74851b0ff043cec7d518
ed.kawas@gmail.com <ed.kawas@gmail.com>
2010-09-29 16:09:55 +0000

    updated the ordering of the definition fields

--- sadi.protege/src/org/sadiframework/editor/DefinitionFieldGeneratorImpl.java
+++ sadi.protege/src/org/sadiframework/editor/DefinitionFieldGeneratorImpl.java
@@ -18,26 +18,26 @@ public class DefinitionFieldGeneratorImpl extends
 		return new DefinitionField[] {
 				new DefinitionField("ServiceName", bundle
 						.getString("definition_name"), TEXT_FIELD ),
+				new DefinitionField("Description", bundle
+				        .getString("definition_description"), TEXT_FIELD),
+				new DefinitionField("InputClass", bundle
+				        .getString("definition_input_class"), DROP_TEXT_FIELD),
+				new DefinitionField("OutputClass", bundle
+				        .getString("definition_output_class"), DROP_TEXT_FIELD),
+		        new DefinitionField("URL", bundle
+		                .getString("definition_endpoint"), TEXT_FIELD),
 				new DefinitionField("Authority", bundle
 						.getString("definition_authority"), TEXT_FIELD),
+		        new DefinitionField("Provider", bundle
+		                .getString("definition_provider"), TEXT_FIELD),
 				new DefinitionField("ServiceType", bundle
 						.getString("definition_service_type"), DROP_TEXT_FIELD),
-				new DefinitionField("InputClass", bundle
-						.getString("definition_input_class"), DROP_TEXT_FIELD),
-				new DefinitionField("OutputClass", bundle
-						.getString("definition_output_class"), DROP_TEXT_FIELD),
-				new DefinitionField("Description", bundle
-						.getString("definition_description"), TEXT_FIELD),
 //				new DefinitionField("UniqueIdentifier", bundle
 //						.getString("definition_unique_id"), TEXT_FIELD),
 				new DefinitionField("Authoritative", bundle
 						.getString("definition_authoritative"), BOOLEAN_FIELD),
-				new DefinitionField("Provider", bundle
-						.getString("definition_provider"), TEXT_FIELD),
 //				new DefinitionField("ServiceURI", bundle
 //						.getString("definition_service_uri"), TEXT_FIELD),
-				new DefinitionField("URL", bundle
-						.getString("definition_endpoint"), TEXT_FIELD),
 //				new DefinitionField("SignatureURL", bundle
 //						.getString("definition_signature_url"), TEXT_FIELD), 
 		};
