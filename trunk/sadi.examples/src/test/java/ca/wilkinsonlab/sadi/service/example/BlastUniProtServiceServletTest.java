2376341e17621a744d00dd769bd4d57e34bff8bb
ben.vvalk <ben.vvalk@e244ecbc-23bd-11de-9d52-39b120432c5d>
2010-10-20 06:33:32 +0000

    blastUniprot service

---
+++ trunk/sadi.examples/src/test/java/ca/wilkinsonlab/sadi/service/example/BlastUniProtServiceServletTest.java
@@ -0,0 +1,43 @@
+package ca.wilkinsonlab.sadi.service.example;
+
+import ca.wilkinsonlab.sadi.service.ServiceServlet;
+import ca.wilkinsonlab.sadi.service.ServiceServletTestBase;
+
+public class BlastUniProtServiceServletTest extends ServiceServletTestBase 
+{
+	@Override
+	protected Object getInput()
+	{
+		return BlastUniProtServiceServletTest.class.getResourceAsStream("/blastUniprot-input.rdf");
+	}
+
+	@Override
+	protected Object getExpectedOutput()
+	{
+		return BlastUniProtServiceServletTest.class.getResourceAsStream("/blastUniprot-output.rdf");
+	}
+
+	@Override
+	protected String getInputURI()
+	{
+		return "http://purl.uniprot.org/uniprot/P12345";
+	}
+	
+	@Override
+	protected String getServiceURI()
+	{
+		return "http://sadiframework.org/examples/blastUniprot";
+	}
+
+	@Override
+	protected String getLocalServiceURL()
+	{
+		return "http://localhost:8080/sadi-examples/blastUniprot";
+	}
+
+	@Override
+	protected ServiceServlet getServiceServletInstance()
+	{
+		return new BlastUniProtServiceServlet();
+	}
+}
