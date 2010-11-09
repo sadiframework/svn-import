036b69391ab72a7b15751a91ad5baf277396fb58
ben.vvalk <ben.vvalk@e244ecbc-23bd-11de-9d52-39b120432c5d>
2010-11-09 19:15:47 +0000

    predicate stats special cases: i) remember response times for specific inputs sizes, when there aren't enough samples to compute a regression line, and ii) compute an average regression line across all predicates, to be used when no stats for a predicate are available

--- trunk/sadi.share/src/main/java/ca/wilkinsonlab/sadi/stats/PredicateStatsDB.java
+++ trunk/sadi.share/src/main/java/ca/wilkinsonlab/sadi/stats/PredicateStatsDB.java
@@ -3,16 +3,15 @@ package ca.wilkinsonlab.sadi.stats;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import net.sf.ehcache.Cache;
 import net.sf.ehcache.Element;
 import net.sf.ehcache.config.CacheConfiguration;
 import net.sf.ehcache.store.MemoryStoreEvictionPolicy;
 
+import org.apache.commons.lang.time.StopWatch;
 import org.apache.commons.math.stat.regression.SimpleRegression;
 import org.apache.commons.configuration.Configuration;
 import org.apache.log4j.Logger;
@@ -51,7 +50,13 @@ public class PredicateStatsDB
 
 	protected final static int INTERNAL_STATS_CACHE_SIZE = 5000;
 	protected final static int UNINITIALIZED = -1; 
-	
+
+	/*
+	 * This value is placed in the statsCache to indicate that we have already queried the
+	 * stats endpoint for predicate X in direction Y, and there are no stats available.
+	 */
+	protected static final SummaryStatsEntry NULL_STATS_ENTRY = new SummaryStatsEntry(null, false, NO_STATS_AVAILABLE, NO_STATS_AVAILABLE, NO_STATS_AVAILABLE);
+
 	protected SPARQLEndpoint endpoint;
 	protected String samplesGraph;
 	protected String statsGraph;
@@ -107,6 +112,9 @@ public class PredicateStatsDB
 	
 	throws IOException
 	{
+		StopWatch stopWatch = new StopWatch();
+		stopWatch.start();
+		
 		endpoint = new VirtuosoSPARQLEndpoint(endpointURL, username, password);
 		
 		initCache();
@@ -124,6 +132,9 @@ public class PredicateStatsDB
 		}
 
 		initNumSamples();
+		
+		stopWatch.stop();
+		log.info(String.format("initialized statsDB singleton in %d milliseconds", stopWatch.getTime()));
 	}
 	
 	protected void initCache() 
@@ -205,29 +216,12 @@ public class PredicateStatsDB
 		
 		log.debug("recomputing summary stats");
 		
-		Set<Property> propertiesToEstimateForward = new HashSet<Property>();
-		Set<Property> propertiesToEstimateReverse = new HashSet<Property>();
-		
 		for(Property p : getAllPredicatesWithSamples()) {
-			
-			if(!recomputeStats(p, true)) {
-				propertiesToEstimateForward.add(p);
-			}
-			if(!recomputeStats(p, false)) {
-				propertiesToEstimateReverse.add(p);
-			}
-		
+			recomputeStats(p, true);
+			recomputeStats(p, false);
 		}
 		
 		recomputeAverageStats();
-		
-		for(Property p : propertiesToEstimateForward) {
-			recomputeEstimatedStats(p, true);
-		}
-		
-		for(Property p : propertiesToEstimateReverse) {
-			recomputeEstimatedStats(p, false);
-		}
 		
 	}
 	
@@ -267,13 +261,31 @@ public class PredicateStatsDB
 			 */
 
 			if(Double.isNaN(regressionModel.getIntercept())) {
-				log.debug(String.format("insufficient samples to calculate regression line for %s, stats will be estimated using available samples and average base time", p.getURI()));
-				return false;
-			}
+
+				/* 
+				 * The number of inputs must be the same for all samples, 
+				 * otherwise we would have been able to compute a
+				 * regression line. 
+				 */
+				int numInputs = Integer.valueOf(results.iterator().next().get("numInputs"));
+				long responseTimeSum = 0;
+
+				log.debug(String.format("unable to compute regression line, all time samples for %s are for the same number of inputs (%d)", p.getURI(), numInputs));
+				
+				for(Map<String,String> binding : results) {
+					responseTimeSum += Integer.valueOf(binding.get("responseTime"));
+				}
+				
+				int averageResponseTime = (int)(responseTimeSum / results.size());
+				recordAverageResponseTime(p, directionIsForward, numInputs, averageResponseTime);
+
+			} else {
 			
-			int estimatedBaseTime = (int)Math.round(Math.max(0, regressionModel.getIntercept()));
-			int estimatedTimePerInput = (int)Math.round(Math.max(0, regressionModel.getSlope()));
-			recordSummaryStats(p, directionIsForward, estimatedBaseTime, estimatedTimePerInput, results.size());
+				int estimatedBaseTime = (int)Math.round(Math.max(0, regressionModel.getIntercept()));
+				int estimatedTimePerInput = (int)Math.round(Math.max(0, regressionModel.getSlope()));
+				recordSummaryStats(p, directionIsForward, estimatedBaseTime, estimatedTimePerInput, results.size());
+
+			}
 			
 		} catch(IOException e) {
 			
@@ -354,55 +366,6 @@ public class PredicateStatsDB
 			
 		} catch(IOException e) {
 
-			log.error("error querying/updating predicate stats db: ", e);
-		}
-		
-	}
-	
-	protected void recomputeEstimatedStats(Property p, boolean directionIsForward) 
-	{
-		try {
-
-			String direction = directionIsForward ? "forward" : "reverse";
-
-			String queryTemplate = SPARQLStringUtils.readFully(PredicateStatsDB.class.getResource("get.samples.by.predicate.sparql.template"));
-			String query = SPARQLStringUtils.strFromTemplate(queryTemplate, this.samplesGraph, p.getURI(), String.valueOf(directionIsForward));
-			List<Map<String,String>> results = endpoint.selectQuery(query);
-			
-			if(results.size() == 0) {
-				log.debug(String.format("no samples for %s in %s direction, stats db will use average base time and average time-per-input to estimate %s", p.getURI(), direction, p.getURI()));
-				return;
-			}
-			
-			log.debug(String.format("computing estimated statistics for %s in %s direction, based on existing samples and average base time", p.getURI(), direction));
-			
-			int numInputsSum = 0;
-			int responseTimeSum = 0;
-			
-			for(Map<String,String> binding : results) {
-
-				int numInputs = Integer.valueOf(binding.get("numInputs"));
-				int responseTime = Integer.valueOf(binding.get("responseTime"));
-
-				numInputsSum += numInputs;
-				responseTimeSum += responseTime;
-				
-			}
-
-			int numInputsAvg = numInputsSum / results.size();
-			int responseTimeAvg = responseTimeSum / results.size();
-			
-			if(getAverageTimePerInput() != NO_STATS_AVAILABLE) {
-
-				int estimatedTimePerInput = getAverageTimePerInput();
-				int estimatedBaseTime = (responseTimeAvg - (estimatedTimePerInput * numInputsAvg)); 
-
-				recordSummaryStats(p, directionIsForward, estimatedBaseTime, estimatedTimePerInput, results.size());
-
-			}
-			
-		} catch(IOException e) {
-			
 			log.error("error querying/updating predicate stats db: ", e);
 		}
 		
@@ -561,6 +524,37 @@ public class PredicateStatsDB
 		}
 		
 	}
+
+	protected void recordAverageResponseTime(Property p, boolean directionIsForward, int numInputs, int averageResponseTime)
+	{
+		try {
+			
+			String responseTimePredicate = directionIsForward ? PredicateStats.AVERAGE_RESPONSE_TIME_FORWARD : PredicateStats.AVERAGE_RESPONSE_TIME_REVERSE;
+
+			log.debug(String.format("computed average response time of %d seconds for %d inputs to %s in the %s direction", 
+					averageResponseTime,
+					numInputs,
+					p.getURI(),
+					directionIsForward ? "forward" : "reverse"));
+
+			String insertTemplate = SPARQLStringUtils.readFully(PredicateStatsDB.class.getResource("record.average.response.time.sparql.template"));
+			
+			String insertQuery = SPARQLStringUtils.strFromTemplate(
+					insertTemplate, 
+					this.statsGraph,
+					p.getURI(),
+					String.valueOf(numInputs),
+					responseTimePredicate,
+					String.valueOf(averageResponseTime));
+			
+			endpoint.updateQuery(insertQuery);
+			
+		} catch(IOException e) {
+			
+			log.error("error updating predicate stats db: ", e);
+		}
+		
+	}
 	
 	protected Collection<Property> getAllPredicatesWithSamples() 
 	{
@@ -615,29 +609,56 @@ public class PredicateStatsDB
 	
 	public int getEstimatedTime(Property predicate, boolean directionIsForward, int numInputs)
 	{
-		StatsEntry statsEntry = getStatsEntry(predicate, directionIsForward);
-			
-		/* there are no stats for the given predicate/direction in the DB */
-		if(statsEntry == null) {
-			
-			try {
-			
-				if(getAverageBaseTime() == NO_STATS_AVAILABLE || getAverageTimePerInput() == NO_STATS_AVAILABLE) {
-					return NO_STATS_AVAILABLE;
-				} else {
-					return (getAverageBaseTime() + (numInputs * getAverageTimePerInput()));
-				}
-			
-			} catch(IOException e){
-			
-				log.error("error occurred querying stats db for average base time and average time-per-input, returning NO_STATS_AVAILABLE instead:", e);
-				return NO_STATS_AVAILABLE;
+		/* 
+		 * If a regression line has been computed for the given predicate and direction,
+		 * we can predict the response time for any number of inputs. 
+		 */
+		
+		SummaryStatsEntry statsEntry = getSummaryStats(predicate, directionIsForward);
 
-			}
-			
+		if(statsEntry != null) {
+			return (statsEntry.getBaseTime() + (numInputs * statsEntry.getTimePerInput()));
+		}
+
+		/* 
+		 * There were insufficient samples to compute a regression line for the given
+		 * predicate and direction, the last time the stats were recomputed.
+		 * 
+		 * However, the stats DB may have samples recorded for exact number of inputs
+		 * the caller is requesting an estimate for.  In this case, we can return the average 
+		 * of those response time of the matching samples as a prediction. 
+		 */
+		
+		if(getAverageResponseTime(predicate, directionIsForward, numInputs) != NO_STATS_AVAILABLE) {
+			return getAverageResponseTime(predicate, directionIsForward, numInputs);
 		}
 		
-		return (statsEntry.getBaseTime() + (numInputs * statsEntry.getTimePerInput()));
+		/*
+		 * If there is no regression line for the given predicate and direction, and
+		 * there is no average response time for numInputs specifically, then use a
+		 * regression representing an "average" service (i.e. average base time and
+		 * average time-per-input).
+		 * 
+		 * Average base time and time-per-input may not be available yet, if there
+		 * aren't sufficient samples to compute a regression line for at least
+		 * one predicate.  If this is the case, then return NO_STATS_AVAILABLE.
+		 */
+		
+		try {
+
+			if(getAverageBaseTime() == NO_STATS_AVAILABLE || getAverageTimePerInput() == NO_STATS_AVAILABLE) {
+				return NO_STATS_AVAILABLE;
+			} else {
+				return (getAverageBaseTime() + (numInputs * getAverageTimePerInput()));
+			}
+
+		} catch(IOException e){
+
+			log.error("error occurred querying stats db for average base time and average time-per-input, returning NO_STATS_AVAILABLE:", e);
+			return NO_STATS_AVAILABLE;
+
+		}
+		
 	}
 	
 	public synchronized void purgeSamples()
@@ -682,7 +703,7 @@ public class PredicateStatsDB
 		return Long.valueOf(firstRow.get("timestamp"));			
 	}
 	
-	protected String getCacheKey(Property p, boolean directionIsForward) 
+	protected String getCacheKeyForSummaryStats(Property p, boolean directionIsForward) 
 	{
 		StringBuilder builder = new StringBuilder();
 		builder.append(p.getURI());
@@ -692,13 +713,32 @@ public class PredicateStatsDB
 		return builder.toString();
 	}
 	
-	protected StatsEntry getStatsEntry(Property p, boolean directionIsForward)
+	protected String getCacheKeyForSamplesAverage(Property p, boolean directionIsForward, int numInputs)
 	{
-		Element cacheEntry = statsCache.get(getCacheKey(p, directionIsForward));
+		StringBuilder builder = new StringBuilder();
+		builder.append(p.getURI());
+		builder.append(":");
+		builder.append(String.valueOf(directionIsForward));
+		builder.append(":");
+		builder.append(String.valueOf(numInputs));
+		
+		return builder.toString();
+	}
+	
+	protected SummaryStatsEntry getSummaryStats(Property p, boolean directionIsForward)
+	{
+		Element cacheEntry = statsCache.get(getCacheKeyForSummaryStats(p, directionIsForward));
 
 		if(cacheEntry != null) {
-			StatsEntry statsEntry = (StatsEntry)(cacheEntry.getObjectValue());
-			return (StatsEntry)(statsEntry.clone());
+			
+			SummaryStatsEntry statsEntry = (SummaryStatsEntry)(cacheEntry.getObjectValue());
+			
+			if(statsEntry.equals(NULL_STATS_ENTRY)) {
+				return null;
+			} else {
+				return (SummaryStatsEntry)(statsEntry.clone());
+			}
+		
 		}
 		
 		try {
@@ -737,28 +777,33 @@ public class PredicateStatsDB
 					
 			List<Map<String,String>> results = endpoint.selectQuery(query);
 			
+			SummaryStatsEntry statsEntry;
+			
 			if(results.size() == 0) {
+			
 				log.debug(String.format("no stats available for %s in the %s direction", p.getURI(), direction));
-				return null;
-			}
+				statsEntry = NULL_STATS_ENTRY;
 			
-			if(results.size() > 1) {
+			} else if(results.size() > 1) {
+			
 				log.error(String.format("stats db is corrupt! (more than one entry for %s in the %s direction)", p.getURI(), direction));
-				return null;
+				statsEntry = NULL_STATS_ENTRY;
+			
+			} else {
+
+				Map<String,String> firstRow = results.iterator().next();
+				int baseTime = Integer.valueOf(firstRow.get("baseTime"));
+				int timePerInput = Integer.valueOf(firstRow.get("timePerInput"));
+				int numSamples = Integer.valueOf(firstRow.get("numSamples"));
+				
+				statsEntry = new SummaryStatsEntry(p.getURI(), directionIsForward, baseTime, timePerInput, numSamples);
+
 			}
 			
-			Map<String,String> firstRow = results.iterator().next();
-			
-			int baseTime = Integer.valueOf(firstRow.get("baseTime"));
-			int timePerInput = Integer.valueOf(firstRow.get("timePerInput"));
-			int numSamples = Integer.valueOf(firstRow.get("numSamples"));
-			
-			StatsEntry statsEntry = new StatsEntry(p.getURI(), directionIsForward, baseTime, timePerInput, numSamples);
-		
-			Element newCacheEntry = new Element(getCacheKey(p, directionIsForward), statsEntry);  
+			Element newCacheEntry = new Element(getCacheKeyForSummaryStats(p, directionIsForward), statsEntry);  
 			statsCache.put(newCacheEntry);
 			
-			return (StatsEntry)(statsEntry.clone());
+			return statsEntry.equals(NULL_STATS_ENTRY) ? null : (SummaryStatsEntry)(statsEntry.clone()); 
 
 		} catch(IOException e) {
 
@@ -767,8 +812,63 @@ public class PredicateStatsDB
 		
 		return null;
 	}
+
+	protected int getAverageResponseTime(Property p, boolean directionIsForward, int numInputs)
+	{
+		Element cacheEntry = statsCache.get(getCacheKeyForSamplesAverage(p, directionIsForward, numInputs));
+
+		if(cacheEntry != null) {
+			return (Integer)(cacheEntry.getObjectValue());
+		}
+		
+		try {
+			
+			String responseTimePredicate = directionIsForward ? PredicateStats.AVERAGE_RESPONSE_TIME_FORWARD : PredicateStats.AVERAGE_RESPONSE_TIME_REVERSE;
+			String direction = directionIsForward ? "forward" : "reverse";
+			
+			String queryTemplate = SPARQLStringUtils.readFully(PredicateStatsDB.class.getResource("get.average.response.time.sparql.template"));
+			
+			String query = SPARQLStringUtils.strFromTemplate(
+					queryTemplate, 
+					this.statsGraph,
+					p.getURI(),
+					String.valueOf(numInputs),
+					responseTimePredicate);
+					
+			List<Map<String,String>> results = endpoint.selectQuery(query);
+			
+			Integer averageResponseTime;
+			
+			if(results.size() == 0) {
+			
+				log.debug(String.format("no response time average available for %d inputs %s in the %s direction", numInputs, p.getURI(), direction));
+				averageResponseTime = NO_STATS_AVAILABLE;
+			
+			} else { 
+			
+				if(results.size() > 1) {
+					log.error(String.format("stats db is corrupt! (more than one response time average is recorded for %d inputs to %s in the %s direction)", numInputs, p.getURI(), direction));
+				}
+
+				Map<String,String> firstRow = results.iterator().next();
+				averageResponseTime = Integer.valueOf(firstRow.get("averageResponseTime"));
+				
+			}
+			
+			Element newCacheEntry = new Element(getCacheKeyForSamplesAverage(p, directionIsForward, numInputs), averageResponseTime);  
+			statsCache.put(newCacheEntry);
+			
+			return averageResponseTime;
+
+		} catch(IOException e) {
+
+			log.error("error querying predicate stats db: ", e);
+		}
+		
+		return NO_STATS_AVAILABLE;
+	}
 	
-	protected static class StatsEntry
+	protected static class SummaryStatsEntry
 	{
 		protected String predicate;
 		protected boolean directionIsForward;
@@ -776,7 +876,7 @@ public class PredicateStatsDB
 		protected int timePerInput;
 		protected int numSamples;
 
-		public StatsEntry(String predicate, boolean directionIsForward, int baseTime, int timePerInput, int numSamples)
+		public SummaryStatsEntry(String predicate, boolean directionIsForward, int baseTime, int timePerInput, int numSamples)
 		{
 			this.predicate = predicate;
 			this.directionIsForward = directionIsForward;
@@ -808,7 +908,7 @@ public class PredicateStatsDB
 		@Override
 		protected Object clone() 
 		{
-			return new StatsEntry(predicate, directionIsForward, baseTime, timePerInput, numSamples);
+			return new SummaryStatsEntry(predicate, directionIsForward, baseTime, timePerInput, numSamples);
 		}
 		
 	}
