74802186dc60b5dabccb2cc4b2cec32d82c50a30
ed.kawas@gmail.com <ed.kawas@gmail.com>
2011-02-02 17:07:21 +0000

    Removed LS::ID references and replaced it with OWL::LSID references.

--- Perl/OWL2Perl/lib/OWL/Base.pm
+++ Perl/OWL2Perl/lib/OWL/Base.pm
@@ -10,7 +10,7 @@ package OWL::Base;
 use strict;
 use HTTP::Date;
 use URI;
-use LS::ID;
+use OWL::LSID;
 
 use vars qw( $VERSION $Revision $AUTOLOAD @EXPORT @ISA );
 use vars qw( $LOG $LOGGER_NAME $CONFIG_NAMESPACE );
@@ -191,7 +191,7 @@ sub uri2package {
 
 	# is $uri an LSID?
 	if ( lc($uri) =~ m/^urn\:lsid/gi ) {
-		my $lsid = LS::ID->new( $uri );
+		my $lsid = OWL::LSID->new( $uri );
 
 		# cheat a little ;-)
 		$uri =
