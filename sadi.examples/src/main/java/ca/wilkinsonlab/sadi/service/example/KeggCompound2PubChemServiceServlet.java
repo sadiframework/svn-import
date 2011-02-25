384959bac2b57266d471c202a68a570e58cc587f
ben.vvalk <ben.vvalk@gmail.com>
2011-02-25 20:52:39 +0000

    reimplemented KEGG services with batching and caching

--- sadi.examples/src/main/java/ca/wilkinsonlab/sadi/service/example/KeggCompound2PubChemServiceServlet.java
+++ sadi.examples/src/main/java/ca/wilkinsonlab/sadi/service/example/KeggCompound2PubChemServiceServlet.java
@@ -1,64 +1,49 @@
 package ca.wilkinsonlab.sadi.service.example;
 
-import java.rmi.RemoteException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.regex.Pattern;
+import java.util.Map;
 
-import javax.xml.rpc.ServiceException;
-
-import keggapi.LinkDBRelation;
-
+import org.apache.commons.lang.text.StrTokenizer;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import ca.wilkinsonlab.sadi.utils.KeggUtils;
 import ca.wilkinsonlab.sadi.utils.ServiceUtils;
 import ca.wilkinsonlab.sadi.vocab.LSRN;
 import ca.wilkinsonlab.sadi.vocab.Properties;
+import ca.wilkinsonlab.sadi.vocab.LSRN.LSRNRecordType;
 
 import com.hp.hpl.jena.rdf.model.Resource;
 
 @SuppressWarnings("serial")
 public class KeggCompound2PubChemServiceServlet extends KeggServiceServlet
 {
+	@SuppressWarnings("unused")
 	private static final Log log = LogFactory.getLog(KeggCompound2PubChemServiceServlet.class);
-	private static final Pattern PUBCHEM_ID_PREFIX = Pattern.compile("^pubchem:", Pattern.CASE_INSENSITIVE);
-	protected static final int RESULTS_LIMIT_PER_QUERY = 200;
+	private static final String DBLINKS_RECORD_SECTION = "DBLINKS";
 	
 	@Override
-	protected void processInput(Resource input, Resource output)
-	{
-		String keggCompoundId = ServiceUtils.getDatabaseId(input, LSRN.KEGG.Compound);
-		
-		if(keggCompoundId == null) {
-			log.error(String.format("unable to determine KEGG compound ID for %s", input));
-			return;
-		}
-		
-		List<LinkDBRelation> results = new ArrayList<LinkDBRelation>();
-		try {
-			
-			LinkDBRelation[] resultsChunk;
-			int startEntry = 1;
-			do {
-				resultsChunk = getKeggService().get_linkdb_by_entry(keggCompoundId, "pubchem", startEntry, RESULTS_LIMIT_PER_QUERY);
-				results.addAll(Arrays.asList(resultsChunk));
-				startEntry += resultsChunk.length;
-			} 
-			while (resultsChunk.length >= RESULTS_LIMIT_PER_QUERY);
-			
-		} catch(ServiceException e) {
-			throw new RuntimeException("error initializing KEGG API service:", e);
-		} catch(RemoteException e) {
-			throw new RuntimeException("error invoking KEGG API service:", e);
-		}
-		
-		for(LinkDBRelation crossref : results) {
-			String pubChemId = PUBCHEM_ID_PREFIX.matcher(crossref.getEntry_id2()).replaceFirst("");
-			Resource keggPubChemNode = ServiceUtils.createLSRNRecordNode(output.getModel(), LSRN.PubChem.Substance, pubChemId);
-			output.addProperty(Properties.isSubstance, keggPubChemNode);
-		}
+	protected LSRNRecordType getInputRecordType() {
+		return LSRN.KEGG.Compound;
 	}
 	
+	@Override
+	protected void processInput(String keggGeneId, String keggGeneRecord, Resource output)
+	{
+		Map<String,String> recordSections = KeggUtils.getSectionsFromKeggRecord(keggGeneRecord);
+		StrTokenizer tokenizer = new StrTokenizer();
+		
+		if(recordSections.containsKey(DBLINKS_RECORD_SECTION)) {
+			for(String line : recordSections.get(DBLINKS_RECORD_SECTION).split("\\r?\\n")) {
+				tokenizer.reset(line);
+				if (tokenizer.nextToken().toLowerCase().equals("pubchem:")) {
+					while(tokenizer.hasNext()) {
+						Resource pubchemNode = ServiceUtils.createLSRNRecordNode(output.getModel(), LSRN.PubChem.Substance, tokenizer.nextToken());
+						output.addProperty(Properties.isSubstance, pubchemNode);
+					}
+					break;
+				}
+			}
+		}
+	}
+
 }
--- sadi.examples/src/main/java/ca/wilkinsonlab/sadi/service/example/KeggServiceServlet.java
+++ sadi.examples/src/main/java/ca/wilkinsonlab/sadi/service/example/KeggServiceServlet.java
@@ -1,21 +1,88 @@
 package ca.wilkinsonlab.sadi.service.example;
 
+import java.rmi.RemoteException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
+
 import javax.xml.rpc.ServiceException;
 
-import keggapi.KEGGLocator;
-import keggapi.KEGGPortType;
-import ca.wilkinsonlab.sadi.service.simple.SimpleAsynchronousServiceServlet;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.Resource;
+
+import ca.wilkinsonlab.sadi.service.AsynchronousServiceServlet;
+import ca.wilkinsonlab.sadi.utils.KeggUtils;
+import ca.wilkinsonlab.sadi.utils.ServiceUtils;
+import ca.wilkinsonlab.sadi.vocab.LSRN.LSRNRecordType;
 
 @SuppressWarnings("serial")
-public abstract class KeggServiceServlet extends SimpleAsynchronousServiceServlet 
+public abstract class KeggServiceServlet extends AsynchronousServiceServlet 
 {
-	KEGGPortType keggService;
+	private static final Log log = LogFactory.getLog(KeggServiceServlet.class);
 	
-	protected synchronized KEGGPortType getKeggService() throws ServiceException 
+	@Override
+	public int getInputBatchSize()
 	{
-		if(keggService == null) {
-			keggService = new KEGGLocator().getKEGGPort();
+		// override input batch size to 1024, the maximum for the KEGG API...
+		return 100;
+	}
+	
+	@Override
+	protected InputProcessingTask getInputProcessingTask(Model inputModel, Collection<Resource> inputNodes)
+	{
+		return new KeggInputProcessingTask(inputModel, inputNodes);
+	}
+	
+	protected abstract void processInput(String keggRecordId, String keggRecord, Resource output);
+	
+	protected abstract LSRNRecordType getInputRecordType();
+
+	protected String getInputIdPrefix() 
+	{
+		return "";
+	}
+
+	private class KeggInputProcessingTask extends InputProcessingTask
+	{	
+		public KeggInputProcessingTask(Model inputModel, Collection<Resource> inputNodes)
+		{
+			super(inputModel, inputNodes);
 		}
-		return keggService;
+
+		public void run()
+		{
+			Map<String, Resource> idToOutputNode = new HashMap<String, Resource>(inputNodes.size());
+			for (Resource inputNode: inputNodes) {
+				String id = ServiceUtils.getDatabaseId(inputNode, getInputRecordType());
+				if(id == null) {
+					log.warn(String.format("skipping input node %s, unable to determine KEGG record ID", inputNode.getURI()));
+					continue;
+				}
+				id = String.format("%s%s", getInputIdPrefix(), id);
+				idToOutputNode.put(id, outputModel.getResource(inputNode.getURI()));
+			}
+			
+			Set<Entry<String,String>> entries;
+			try {
+				entries = KeggUtils.getKeggRecords(idToOutputNode.keySet()).entrySet();
+			} catch(ServiceException e) {
+				throw new RuntimeException("error initializing KEGG API", e);
+			} catch(RemoteException e) {
+				throw new RuntimeException("error contacting KEGG service", e);
+			}
+			
+			log.debug(String.format("retrieved %d entries", entries.size()));
+			for (Entry<String,String> entry: entries) {
+				String keggId = StringUtils.removeStart(entry.getKey(), getInputIdPrefix());
+				processInput(keggId, entry.getValue(), idToOutputNode.get(entry.getKey()));
+			}
+			success();
+		}
 	}
 }
