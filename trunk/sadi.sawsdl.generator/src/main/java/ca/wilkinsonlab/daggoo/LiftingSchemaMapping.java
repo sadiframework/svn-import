1f501bb576429d98177b0509f0ba134ebae6811e
ed.kawas@gmail.com <ed.kawas@gmail.com@e244ecbc-23bd-11de-9d52-39b120432c5d>
2011-03-11 17:08:37 +0000

    added RDFPaths to the lifting schema mapping

--- trunk/sadi.sawsdl.generator/src/main/java/ca/wilkinsonlab/daggoo/LiftingSchemaMapping.java
+++ trunk/sadi.sawsdl.generator/src/main/java/ca/wilkinsonlab/daggoo/LiftingSchemaMapping.java
@@ -1,13 +1,24 @@
 package ca.wilkinsonlab.daggoo;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import ca.wilkinsonlab.daggoo.utils.Base64;
+import ca.wilkinsonlab.sadi.rdfpath.RDFPath;
 
 public class LiftingSchemaMapping extends SchemaMapping {
 
     List<LiftingMap> liftingMap;
+    Map<String, RDFPath> rdfPaths = new HashMap<String, RDFPath>();
+    
     public LiftingSchemaMapping() {
 	super();
+	rdfPaths = new HashMap<String, RDFPath>();
     }
     
     public List<LiftingMap> getLiftingMap() {
@@ -21,6 +32,37 @@ public class LiftingSchemaMapping extends SchemaMapping {
 	if (this.liftingMap == null)
 	    setLiftingMap(new ArrayList<LiftingMap>());
         liftingMap.add(mapping);
+    }
+    
+    @Override
+    public void setTemplate(String template) {
+        super.setTemplate(template);
+        processTemplate();
+    }
+
+    private void processTemplate() {
+	rdfPaths = new HashMap<String, RDFPath>();
+	Properties p = new Properties();
+	try {
+	    p.load(new ByteArrayInputStream(getTemplate().getBytes()));
+	} catch (IOException e) {
+	    // TODO log this error
+	}
+	for (Object key : p.keySet()) {
+	    String s = p.getProperty(key.toString());
+	    String[] parts = s.split(",");
+	    // each part is base64 encoded ... except for the last element
+	    for (int i = 0; i < parts.length-1; i++) {
+		parts[i] = new String(Base64.base64ToByteArray(parts[i]));
+	    }
+	    RDFPath path = new RDFPath(parts);
+	    path.reuseExistingNodes = false;
+	    rdfPaths.put(key.toString(), path);
+	}
+    }
+
+    public Map<String, RDFPath> getRdfPaths() {
+        return rdfPaths;
     }
     
 }
