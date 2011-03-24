bb7a421707beba187b2209a61480fdd05d8994e3
ed.kawas@gmail.com <ed.kawas@gmail.com@e244ecbc-23bd-11de-9d52-39b120432c5d>
2011-03-24 15:15:22 +0000

    removed the base64 encoding/decoding

--- trunk/sadi.sawsdl.generator/src/main/java/ca/wilkinsonlab/daggoo/LiftingSchemaMapping.java
+++ trunk/sadi.sawsdl.generator/src/main/java/ca/wilkinsonlab/daggoo/LiftingSchemaMapping.java
@@ -1,14 +1,15 @@
 package ca.wilkinsonlab.daggoo;
 
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Properties;
 
-import ca.wilkinsonlab.daggoo.utils.Base64;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+
 import ca.wilkinsonlab.sadi.rdfpath.RDFPath;
 
 public class LiftingSchemaMapping extends SchemaMapping {
@@ -42,20 +43,32 @@ public class LiftingSchemaMapping extends SchemaMapping {
 
     private void processTemplate() {
 	rdfPaths = new HashMap<String, RDFPath>();
-	Properties p = new Properties();
+	PropertiesConfiguration p = new PropertiesConfiguration();
 	try {
 	    p.load(new ByteArrayInputStream(getTemplate().getBytes()));
-	} catch (IOException e) {
-	    // TODO log this error
+	} catch (ConfigurationException e) {
+	    e.printStackTrace();
+	    return;
 	}
-	for (Object key : p.keySet()) {
-	    String s = p.getProperty(key.toString());
-	    String[] parts = s.split(",");
-	    // each part is base64 encoded ... except for the last element
-	    for (int i = 0; i < parts.length-1; i++) {
-		parts[i] = new String(Base64.base64ToByteArray(parts[i]));
+	Iterator<?> it = p.getKeys();
+	while (it.hasNext()) {
+	    String key = it.next() + "";
+	    String[] parts = new String[]{};
+	    RDFPath path;
+	    if (p.getProperty(key) instanceof String) {
+		String s = (String)p.getProperty(key); 
+		parts = s.split(",");
+		path = new RDFPath(parts);
+	    } else if (p.getProperty(key) instanceof List){
+		List s = (List)p.getProperty(key);
+		parts = new String[s.size()];
+		for (int i = 0; i < parts.length; i++) {
+		    parts[i] = s.get(i) +"";
+		}
+		path = new RDFPath(parts);
+	    } else {
+		continue;
 	    }
-	    RDFPath path = new RDFPath(parts);
 	    path.reuseExistingNodes = false;
 	    rdfPaths.put(key.toString(), path);
 	}
