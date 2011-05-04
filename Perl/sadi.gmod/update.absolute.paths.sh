11c50655ba937401aefa9dff8043de381aa2b084
ben.vvalk <ben.vvalk@gmail.com>
2011-05-04 22:07:42 +0000

    make update.absolute.paths.sh less dangerous

--- Perl/sadi.gmod/update.absolute.paths.sh
+++ Perl/sadi.gmod/update.absolute.paths.sh
@@ -5,4 +5,4 @@ if [ $# -lt 2 ]; then
     exit 1;
 fi
 
-find . -not -name README -not \( -name .svn -prune \) | perl -ne 'chomp; print "$_\0" if -f' | xargs -0 perl -i -pe "s|$1|$2|g"
+perl -i -pe "s|$1|$2|g" log4perl.properties sadi-services.cfg cgi/*
