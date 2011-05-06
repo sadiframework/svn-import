11eacc08b48117c359445c32c4d832d24ac346d6
ben.vvalk <ben.vvalk@gmail.com>
2011-05-06 22:35:04 +0000

    rollback logging workaround -- somehow causes service script to hang under mod_perl (!?)

--- Perl/sadi.gmod/lib/Utils/Chado/Dbxref.pm
+++ Perl/sadi.gmod/lib/Utils/Chado/Dbxref.pm
@@ -7,7 +7,6 @@ use warnings;
 # imports
 #------------------------------------------------------------
 
-use Log::Log4perl;
 use DBI;
 #use Config::Simple;
 use File::Spec::Functions qw(catfile);
@@ -63,7 +62,7 @@ my $DBNAME_TO_URIPREFIX = LoadFile($DBNAME_TO_URIPREFIX_MAPPING_FILE);
 # logging
 #------------------------------------------------------------
 
-my $LOG = Log::Log4perl->get_logger('services');
+my $LOG = Log::Log4perl->get_logger(__PACKAGE__);
 
 #------------------------------------------------------------
 # subroutines
