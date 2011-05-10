640028ce07d88fe5971c376b150836d762820ab0
elmccarthy <elmccarthy@e244ecbc-23bd-11de-9d52-39b120432c5d>
2011-05-10 20:48:23 +0000

    0.1.0 release

---
+++ tags/0.1.0/sadi.service/src/main/java/ca/wilkinsonlab/sadi/service/annotations/ServiceProvider.java
@@ -0,0 +1,14 @@
+package ca.wilkinsonlab.sadi.service.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Annotation holding the contact email address for the service.
+ * @author Luke McCarthy
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ServiceProvider
+{
+	String value();
+}
