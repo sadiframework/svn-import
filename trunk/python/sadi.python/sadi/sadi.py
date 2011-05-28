a065c62130bfb0a71935756f3501f9397d604f01
mccusker@gmail.com <mccusker@gmail.com@e244ecbc-23bd-11de-9d52-39b120432c5d>
2011-05-28 00:46:45 +0000

    initial Google App Engine support (experimental), modifications based on SADI training session at UNB.

--- trunk/python/sadi.python/sadi/sadi.py
+++ trunk/python/sadi.python/sadi/sadi.py
@@ -1,18 +1,22 @@
 from rdflib import *
 import rdflib
 
-from twisted.internet import reactor
-from twisted.web import server
-import twisted.web.resource
-from twisted.web.static import File
-
-import tempfile
-import os
+googleAppEngine = False
+try:
+    from twisted.internet import reactor
+    from twisted.web import server
+    import twisted.web.resource
+    from twisted.web.static import File
+except:
+    googleAppEngine = True
+    from google.appengine.ext import webapp
+    from google.appengine.ext.webapp.util import run_wsgi_app
+    sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                 'gae/lib/python2.5/site-packages/'))
 from surf import *
 
 from StringIO import StringIO
 
-import platform
 
 rdflib.plugin.register('sparql', rdflib.query.Processor,
                        'rdfextras.sparql.processor', 'Processor')
@@ -22,11 +26,15 @@ rdflib.plugin.register('sparql', rdflib.query.Result,
 ns.register(myGrid="http://www.mygrid.org.uk/mygrid-moby-service#")
 ns.register(protegedc="http://protege.stanford.edu/plugins/owl/dc/protege-dc.owl#")
 
+formats= {
+    "text/n3":"n3",
+    "application/rdf+xml":"xml",
+}
+
 # Install required libraries using easy_install:
 # sudo easy_install 'rdflib>=3.0' surf rdfextras surf.rdflib
 
-class Service(twisted.web.resource.Resource):
-    isLeaf=True
+class ServiceBase:
     serviceDescription = None
 
     comment = None
@@ -34,7 +42,6 @@ class Service(twisted.web.resource.Resource):
     serviceNameText = None
     label = None
     name = None
-    port = None
 
     def getClass(self, identifier):
         return self.descriptionSession.get_class(identifier)
@@ -81,6 +88,13 @@ class Service(twisted.web.resource.Resource):
             inputParameter.mygrid_objectType = self.inputClass
             inputParameter.save()
 
+            if "getParameterClass" in dir(self):
+                self.parameterClass = self.getParameterClass()
+                secondaryParameter = self.Parameter("#params")
+                desc.mygrid_hasOperation[0].mygrid_secondaryParameter = secondaryParameter
+                secondaryParameter.mygrid_objectType = self.parameterClass
+                secondaryParameter.save()
+
             desc.mygrid_hasOperation[0].mygrid_outputParameter = outputParameter
             desc.mygrid_hasOperation[0].mygrid_inputParameter = inputParameter
             desc.mygrid_hasOperation[0].save()
@@ -89,39 +103,69 @@ class Service(twisted.web.resource.Resource):
 
         return self.serviceDescription
 
-    def render_GET(self, request):
-        modelGraph = self.getServiceDescription()
-        request.setHeader("Content-Type","application/rdf+xml")
-        return modelGraph.serialize(format="pretty-xml")
+    def getInstances(self, session, store, graph):
+        InputClass = session.get_class(self.getInputClass())
+        instances = InputClass.all()
 
-    def render_POST(self, request):
-        content = request.content.getvalue()
+    def processGraph(self,content):
         inputStore = Store(reader="rdflib", writer="rdflib",
                            rdflib_store='IOMemory')
         inputSession = Session(inputStore)
         inputStore.reader.graph.parse(StringIO(content))
-        InputClass = inputSession.get_class(self.getInputClass())
         outputStore = Store(reader="rdflib", writer="rdflib",
                             rdflib_store='IOMemory')
         outputSession = Session(outputStore)
         OutputClass = outputSession.get_class(self.getOutputClass())
 
-        instances = InputClass.all()
+        instances = self.getInstances(inputSession, inputStore,
+                                      inputStore.reader.graph)
+        print len(instances)
         for i in instances:
             print i.subject
             o = OutputClass(i.subject)
             self.process(i, o)
+        return outputStore.reader.graph
 
-        request.setHeader("Content-Type","application/rdf+xml")
-        return outputStore.reader.graph.serialize(format="pretty-xml")
+if googleAppEngine:
+    class GAEService(ServiceBase, webapp.RequestHandler):
+        def get(self):
+            modelGraph = self.getServiceDescription()
+            self.response.headers.add_header("Content-Type",
+                                             "application/rdf+xml")
+            self.response.write(modelGraph.serialize(format="pretty-xml"))
+            
+            def post(self):
+                content = self.request.get('content')
+                graph = self.processGraph(content)
+                self.request.add_header("Content-Type","application/rdf+xml")
+                self.response.write(graph.serialize(format="pretty-xml"))
+    Service = GAEService
+else:
+    class TwistedService(ServiceBase, twisted.web.resource.Resource):
+        isLeaf=True
+        
+        def render_GET(self, request):
+            modelGraph = self.getServiceDescription()
+            request.setHeader("Content-Type","application/rdf+xml")
+            return modelGraph.serialize(format="pretty-xml")
+        
+        def render_POST(self, request):
+            content = request.content.read()
+            print content
+            graph = self.processGraph(content)
+            request.setHeader("Content-Type","application/rdf+xml")
+            return graph.serialize(format="pretty-xml")
 
-def getDefaultBaseURI(port=8080):
-    return "http://"+platform.node()+":"+str(port)+"/"
+    Service = TwistedService
 
-def publishService(service, port=8080):
+def publishTwistedService(service, port=8080):
     root = twisted.web.resource.Resource()
-    service.port = port
     root.putChild(service.name, service)
     site = server.Site(root)
     reactor.listenTCP(port, site)
     reactor.run()
+
+def publishAppEngineService(serviceClass,debug=True):
+    application = webapp.WSGIApplication([('/'+serviceClass.name,serviceClass)],
+                                         debug=True)
+    run_wsgi_app(application)
