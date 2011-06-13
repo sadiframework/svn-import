827d9e792b5262c1c8ccb50674c108b8cbcd9298
elmccarthy <elmccarthy@e244ecbc-23bd-11de-9d52-39b120432c5d>
2011-06-13 20:08:00 +0000

    Added ContentType.getUniqueContentTypes() and .readModel(model, stream, base). Added RdfUtils.isURL(string) because RdfUtils.isURI(string) doesn't work quite as expected. Moved TestCase into sadi.common from sadi-service-tester plugin. Added RdfUtils.loadModelFromString to load RDF from remote URL, classpath resource, local file or inline RDF as appropriate.

--- trunk/sadi.common/src/main/java/ca/wilkinsonlab/sadi/utils/ContentType.java
+++ trunk/sadi.common/src/main/java/ca/wilkinsonlab/sadi/utils/ContentType.java
@@ -1,9 +1,16 @@
 package ca.wilkinsonlab.sadi.utils;
 
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.Writer;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.RDFReader;
 import com.hp.hpl.jena.rdf.model.RDFWriter;
 
 public enum ContentType
@@ -19,6 +26,34 @@ public enum ContentType
 				return type;
 		}
 		return null;
+	}
+	
+	private static Collection<ContentType> uniqueContentTypes = null;
+	public static Collection<ContentType> getUniqueContentTypes()
+	{
+		if (uniqueContentTypes == null) {
+			uniqueContentTypes = new ArrayList<ContentType>(ContentType.values().length);
+			Set<String> seen = new HashSet<String>();
+			for (ContentType type: ContentType.values()) {
+				if (!seen.contains(type.getJenaLanguage())) {
+					uniqueContentTypes.add(type);
+					seen.add(type.getJenaLanguage());
+				}
+			}
+		}
+		return uniqueContentTypes;
+	}
+	
+	private static Set<String> uniqueJenaLanguages = null;
+	public static Iterable<String> getUniqueJenaLanguages()
+	{
+		if (uniqueJenaLanguages == null) {
+			Set<String> langs = new HashSet<String>();
+			for (ContentType type: ContentType.values())
+				langs.add(type.getJenaLanguage());
+			uniqueJenaLanguages = Collections.unmodifiableSet(langs);
+		}
+		return uniqueJenaLanguages;
 	}
 	
 	private final String httpHeader;
@@ -48,8 +83,13 @@ public enum ContentType
 	
 	public void writeModel(Model model, OutputStream out, String base)
 	{
-
 		RDFWriter rdf = model.getWriter(getJenaLanguage());
 		rdf.write(model, out, base);
+	}
+	
+	public void readModel(Model model, InputStream in, String base)
+	{
+		RDFReader rdf = model.getReader(getJenaLanguage());
+		rdf.read(model, in, base);
 	}
 }
