f999cdfcc5b5f5e02361555965c8ec40515b1fb1
ben.vvalk <ben.vvalk@e244ecbc-23bd-11de-9d52-39b120432c5d>
2011-07-14 18:32:52 +0000

    import new SADI::Simple based version of sadi.gmod

---
+++ trunk/Perl/sadi.gmod/lib/Utils/LSRN.pm
@@ -0,0 +1,48 @@
+package Utils::LSRN;
+
+use Vocab::SIO qw(
+    HAS_ATTRIBUTE
+    HAS_VALUE
+);
+use Vocab::RDF qw(
+    RDF_TYPE
+);
+use Vocab::LSRN qw(
+    LSRN_ENTITY_PREFIX
+    LSRN_ONTOLOGY_PREFIX
+);
+
+require Exporter;
+
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw(get_id_from_lsrn_record_node);
+
+sub get_id_from_lsrn_record_node
+{
+    my ($model, $record_node) = @_;
+   
+    return undef unless $record_node->is_resource;
+
+    if (!$model->objects($record_node, $has_attribute)) {
+        return undef if $record_node->is_blank;
+        if ($record_node->uri =~ /^@{[LSRN_ENTITY_PREFIX]}(.*?):(.*)$/) {
+            return ($1 . '_Identifier', $2);
+        } else {
+            return undef;
+        }       
+    } 
+
+    foreach my $attribute_node ($model->objects($record_node, HAS_ATTRIBUTE)) {
+        foreach my $attribute_type ($model->objects($attribute_node, RDF_TYPE)) {
+            next unless $attribute_type->is_resource and !$attribute_type->is_blank;
+            if ($attribute_type->uri =~ /^@{[LSRN_ONTOLOGY_PREFIX]}.*_Identifier$/) {
+                my ($id_literal) = $model->objects($attribute_node, HAS_VALUE);
+                return ($attribute_type->uri, $id_literal->value);
+            }
+        }
+    }
+
+}
+
+1;
+
