b97682441ab402965cf6f0ae442a236010897d67
elmccarthy <elmccarthy@e244ecbc-23bd-11de-9d52-39b120432c5d>
2011-08-25 05:09:44 +0000

    Updated to version 0.2.0-SNAPSHOT. Updated BLAST services to match new BLAST RDF model. Updated SIO vocab and added blast.owl for new BLAST services.

--- trunk/sadi.service.examples/src/test/java/ca/wilkinsonlab/sadi/service/example/BlastUniProtServiceServletIT.java
+++ trunk/sadi.service.examples/src/test/java/ca/wilkinsonlab/sadi/service/example/BlastUniProtServiceServletIT.java
@@ -2,12 +2,12 @@ package ca.wilkinsonlab.sadi.service.example;
 
 import org.apache.log4j.Logger;
 
-import com.hp.hpl.jena.rdf.model.Model;
-
 import ca.wilkinsonlab.sadi.SADIException;
 import ca.wilkinsonlab.sadi.client.ServiceImpl;
 import ca.wilkinsonlab.sadi.service.ServiceServletTestBase;
 import ca.wilkinsonlab.sadi.service.annotations.URI;
+
+import com.hp.hpl.jena.rdf.model.Model;
 
 @URI("http://localhost:8180/sadi-examples/blastUniprot")
 public class BlastUniProtServiceServletIT extends ServiceServletTestBase 
@@ -15,7 +15,23 @@ public class BlastUniProtServiceServletIT extends ServiceServletTestBase
 	private static final Logger log = Logger.getLogger(BlastUniProtServiceServletIT.class);
 	
 	@Override
-	protected void sanityCheckOutput(ServiceImpl service, Model output) throws SADIException {
+	protected void sanityCheckOutput(ServiceImpl service, Model output) throws SADIException
+	{
 		log.info("skipping sanity check for memory reasons");
+		return;
 	}
+	
+//	@Override
+//	public void testLocalService() throws SADIException
+//	{
+//		ServiceImpl service = getLocalServiceInstance();
+//		Model output = ModelFactory.createDefaultModel();
+//		output.read("file:/Users/luke/Downloads/blast-uniprot.2.rdf");
+//		output.write(System.out, "RDF/XML-ABBREV");
+//		Resource blasted = output.getResource("http://purl.uniprot.org/core/#_C");
+//		for (Statement s: blasted.listProperties().toList()) {
+//			System.out.println(s);
+//		}
+//		super.sanityCheckOutput(service, output);
+//	}
 }
