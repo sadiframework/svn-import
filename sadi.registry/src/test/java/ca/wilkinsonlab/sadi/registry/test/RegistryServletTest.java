d29eff8719e1baa5f1b3edbcd56534978febd70f
elmccarthy <elmccarthy@gmail.com>
2011-08-25 02:04:06 +0000

    Added a couple of applications for validating services.

--- sadi.registry/src/test/java/ca/wilkinsonlab/sadi/registry/test/RegistryServletTest.java
+++ sadi.registry/src/test/java/ca/wilkinsonlab/sadi/registry/test/RegistryServletTest.java
@@ -1,13 +1,26 @@
 package ca.wilkinsonlab.sadi.registry.test;
 
-import ca.wilkinsonlab.sadi.client.Config;
-import ca.wilkinsonlab.sadi.client.Service;
+import ca.wilkinsonlab.sadi.SADIException;
+import ca.wilkinsonlab.sadi.client.ServiceFactory;
+import ca.wilkinsonlab.sadi.registry.Registry;
+import ca.wilkinsonlab.sadi.service.validation.ServiceValidator;
+
+import com.hp.hpl.jena.rdf.model.ResIterator;
+import com.hp.hpl.jena.rdf.model.Resource;
 
 public class RegistryServletTest
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws SADIException
 	{
-		for (Service s: Config.getConfiguration().getMasterRegistry().findServicesByPredicate("http://sadiframework.org/examples/bmi.owl#BMI"))
-			System.out.println(s);
+		for (ResIterator i = Registry.getRegistry().getRegisteredServiceNodes(); i.hasNext(); ) {
+			Resource service = i.next();
+			System.out.println("validating service " + service);
+			try {
+				ServiceValidator.validateService(service);
+				ServiceFactory.createService(service.getURI()).getInputClass();
+			} catch (SADIException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
