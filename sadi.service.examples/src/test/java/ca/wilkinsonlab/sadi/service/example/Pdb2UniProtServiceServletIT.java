89986d8b1110161228d1b10230e15c5348d284d8
ben.vvalk <ben.vvalk@gmail.com>
2011-10-13 01:07:13 +0000

    tagging 0.1.3 release of sadi.gmod

--- sadi.service.examples/src/test/java/ca/wilkinsonlab/sadi/service/example/Pdb2UniProtServiceServletIT.java
+++
@@ -1,9 +0,0 @@
-package ca.wilkinsonlab.sadi.service.example;
-
-import ca.wilkinsonlab.sadi.service.ServiceServletTestBase;
-import ca.wilkinsonlab.sadi.service.annotations.URI;
-
-@URI("http://localhost:8180/sadi-examples/pdb2uniprot")
-public class Pdb2UniProtServiceServletIT extends ServiceServletTestBase 
-{
-}
