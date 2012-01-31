954de134c4aa5235209cac12decbc0a401fbd742
ben.vvalk <ben.vvalk@gmail.com>
2012-01-31 19:58:46 +0000

    tag wiki version for initial submission of SADI RFC (draft-bvandervalk-sadi-00)

--- sadi.service.examples/src/test/java/ca/wilkinsonlab/sadi/service/example/EntrezGene2KeggServiceServletIT.java
+++
@@ -1,9 +0,0 @@
-package ca.wilkinsonlab.sadi.service.example;
-
-import ca.wilkinsonlab.sadi.service.ServiceServletTestBase;
-import ca.wilkinsonlab.sadi.service.annotations.URI;
-
-@URI("http://localhost:8180/sadi-examples/entrezGene2Kegg")
-public class EntrezGene2KeggServiceServletIT extends ServiceServletTestBase 
-{
-}
