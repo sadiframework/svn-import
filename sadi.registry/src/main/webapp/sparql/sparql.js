e392f015a5799dddd0b61bfea5afbf47b318cd74
elmccarthy <elmccarthy@gmail.com>
2012-04-27 07:26:53 +0000

    [maven-release-plugin]  copy for tag sadi-parent-0.3.0

--- sadi.registry/src/main/webapp/sparql/sparql.js
+++
@@ -1,6 +0,0 @@
-google.load("jquery", "1.3.2");
-google.setOnLoadCallback(function() {
-	jQuery.getScript("../js/jquery.tablesorter.min.js", function() {
-		$("#sparql-results-table").tablesorter(); // { sortList: [[0,0]] }
-	});
-});
--- sadi.service.examples/src/test/java/ca/wilkinsonlab/sadi/service/example/UniProt2PrositeServiceServletIT.java
+++
@@ -1,9 +0,0 @@
-package ca.wilkinsonlab.sadi.service.example;
-
-import ca.wilkinsonlab.sadi.service.ServiceServletTestBase;
-import ca.wilkinsonlab.sadi.service.annotations.URI;
-
-@URI("http://localhost:8180/sadi-examples/uniprot2prosite")
-public class UniProt2PrositeServiceServletIT extends ServiceServletTestBase 
-{
-}
