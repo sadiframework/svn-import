7927a3f0e1bcedd5ed80718c9d94c03cb9f0bc96
elmccarthy <elmccarthy@gmail.com>
2012-05-02 10:42:38 +0000

    [maven-release-plugin]  copy for tag sadi-common-0.3.0

--- sadi.service.examples/src/test/java/ca/wilkinsonlab/sadi/service/example/LinearRegressionServiceServletIT.java
+++
@@ -1,9 +0,0 @@
-package ca.wilkinsonlab.sadi.service.example;
-
-import ca.wilkinsonlab.sadi.service.ServiceServletTestBase;
-import ca.wilkinsonlab.sadi.service.annotations.URI;
-
-@URI("http://localhost:8180/sadi-examples/linear")
-public class LinearRegressionServiceServletIT extends ServiceServletTestBase
-{
-}
--- sadi.service.examples/src/test/java/ca/wilkinsonlab/sadi/service/example/UniProtInfoServiceServletIT.java
+++
@@ -1,9 +0,0 @@
-package ca.wilkinsonlab.sadi.service.example;
-
-import ca.wilkinsonlab.sadi.service.ServiceServletTestBase;
-import ca.wilkinsonlab.sadi.service.annotations.URI;
-
-@URI("http://localhost:8180/sadi-examples/uniprotInfo")
-public class UniProtInfoServiceServletIT extends ServiceServletTestBase
-{
-}
