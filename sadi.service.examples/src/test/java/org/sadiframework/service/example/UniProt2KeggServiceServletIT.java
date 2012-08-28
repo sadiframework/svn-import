264d6d4da3d0460c9e45026ff7193bfc01e1980b
elmccarthy <elmccarthy@gmail.com>
2012-08-28 20:20:04 +0000

    [maven-release-plugin]  copy for tag sadi-parent-0.3.1

--- sadi.service.examples/src/test/java/org/sadiframework/service/example/UniProt2KeggServiceServletIT.java
+++
@@ -1,9 +0,0 @@
-package org.sadiframework.service.example;
-
-import org.sadiframework.service.ServiceServletTestBase;
-import org.sadiframework.service.annotations.URI;
-
-@URI("http://localhost:8180/uniprot2kegg")
-public class UniProt2KeggServiceServletIT extends ServiceServletTestBase 
-{
-}
