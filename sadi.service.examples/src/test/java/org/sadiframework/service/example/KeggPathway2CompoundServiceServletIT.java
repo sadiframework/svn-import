0087e2cfa0233c6b871899442bb20ddb0b451d68
elmccarthy <elmccarthy@gmail.com>
2012-08-28 21:17:41 +0000

    [maven-release-plugin]  copy for tag 0.3.1/sadi-parent

--- sadi.service.examples/src/test/java/org/sadiframework/service/example/KeggPathway2CompoundServiceServletIT.java
+++
@@ -1,9 +0,0 @@
-package org.sadiframework.service.example;
-
-import org.sadiframework.service.ServiceServletTestBase;
-import org.sadiframework.service.annotations.URI;
-
-@URI("http://localhost:8180/keggPathway2Compound")
-public class KeggPathway2CompoundServiceServletIT extends ServiceServletTestBase 
-{
-}
