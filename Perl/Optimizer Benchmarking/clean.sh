c353f0181bcac71c55d88c5c5282d3abfdc52523
elmccarthy <elmccarthy@gmail.com>
2012-09-12 04:27:48 +0000

    Create a tag for the version of ie_sadi_services that sadi.service.nlp was based on.

--- Perl/Optimizer Benchmarking/clean.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-rm *.args *.ordering.? *.log *.results *.time *.exit.status *.last *.complete 2>/dev/null
--- sadi.service.examples/src/test/java/org/sadiframework/service/example/Gene2UniProtServiceServletIT.java
+++
@@ -1,9 +0,0 @@
-package org.sadiframework.service.example;
-
-import org.sadiframework.service.ServiceServletTestBase;
-import org.sadiframework.service.annotations.URI;
-
-@URI("http://localhost:8180/gene2uniprot")
-public class Gene2UniProtServiceServletIT extends ServiceServletTestBase
-{
-}
