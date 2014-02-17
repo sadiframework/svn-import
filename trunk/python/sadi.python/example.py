2213742feca1502056476460abdfc437e45e230d
mccusker@gmail.com <mccusker@gmail.com@e244ecbc-23bd-11de-9d52-39b120432c5d>
2014-02-17 00:00:58 +0000

    Added Asynchronous SADI support. We now do everything that the Java implementation does.

--- trunk/python/sadi.python/example.py
+++ trunk/python/sadi.python/example.py
@@ -1,8 +1,38 @@
 import sadi
 from rdflib import *
+from time import sleep
+
 
 hello=Namespace("http://sadiframework.org/examples/hello.owl#")
 foaf=Namespace("http://xmlns.com/foaf/0.1/")
+
+class ExampleAsyncService(sadi.Service):
+    label = "Hello, world"
+    serviceDescriptionText = 'A simple asynchronous "Hello, World" service that reads a name and attaches a greeting.'
+    comment = 'A simple asynchronous "Hello, World" service that reads a name and attaches a greeting.'
+    serviceNameText = "Hello, world (async python)"
+    name = "example"
+
+    def getOrganization(self):
+        result = self.Organization()
+        result.add(RDFS.label,Literal("Tetherless World Constellation, RPI"))
+        result.add(sadi.mygrid.authoritative, Literal(False))
+        result.add(sadi.dc.creator, URIRef('mailto:mccusker@gmail.com'))
+        return result
+
+    def getInputClass(self):
+        return hello.NamedIndividual
+
+    def getOutputClass(self):
+        return hello.GreetedIndividual
+
+    def async_process(self, i,o):
+        # This is just to make it actually take a bit of time.
+        sleep(7)
+        print input
+        o.set(hello.greeting, Literal("Hello, "+i.value(foaf.name).value))
+
+async_resource = ExampleAsyncService()
 
 class ExampleService(sadi.Service):
     label = "Hello, world"
