8dd4994c58fd43321644a91b32698e6e3a6c40c1
mccusker@gmail.com <mccusker@gmail.com>
2014-03-21 15:50:27 +0000

    Added FRIR and extra serialization hooks.

--- python/sadi.python/setup.py
+++ python/sadi.python/setup.py
@@ -4,7 +4,7 @@ use_setuptools()
 from setuptools import setup, find_packages
 setup(
     name = "sadi",
-    version = "0.5",
+    version = "0.5.1",
     packages = find_packages(exclude=['test.py',"example.py"]),
 
     install_requires = ['rdflib>=4.0', 'rdfextras', 'webob', 'python-dateutil','pytidylib'],
